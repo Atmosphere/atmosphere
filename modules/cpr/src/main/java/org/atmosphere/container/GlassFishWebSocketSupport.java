b77dbb96cef5c19f164bb35cfa977c6ab213d3f1
jfarcand <jfarcand@apache.org>
2010-05-11 22:58:24 -0400

    Unuset import

--- modules/cpr/src/main/java/org/atmosphere/container/GlassFishWebSocketSupport.java
+++ modules/cpr/src/main/java/org/atmosphere/container/GlassFishWebSocketSupport.java
@@ -61,7 +61,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.logging.Level;
 
 /**
