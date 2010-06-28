22c2f25bf6224061722cfaf450a17caa355b091c
jfarcand <jfarcand@apache.org>
2010-06-28 10:35:44 -0400

    Add a unit test for the 204 status code and SuspendResponse scenario

--- samples/jquery-pubsub/src/main/java/org/atmosphere/samples/pubsub/JQueryPubSub.java
+++ samples/jquery-pubsub/src/main/java/org/atmosphere/samples/pubsub/JQueryPubSub.java
@@ -43,7 +43,6 @@ import org.atmosphere.jersey.SuspendResponse;
 
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
-import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
