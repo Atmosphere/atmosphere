1b35bcca391103c88b98c60864c5d2604bae3c4a
jfarcand <jfarcand@apache.org>
2010-09-13 20:40:22 -0400

    Get rid of IDEA generated comments

--- modules/cpr/src/main/java/org/atmosphere/websocket/WebSocketHttpServletResponse.java
+++ modules/cpr/src/main/java/org/atmosphere/websocket/WebSocketHttpServletResponse.java
@@ -76,147 +76,147 @@ public class WebSocketHttpServletResponse<A extends WebSocketSupport> extends Ht
         super(new HttpServletResponse() {
 
             public void addCookie(Cookie cookie) {
-                //To change body of implemented methods use File | Settings | File Templates.
+                
             }
 
             public boolean containsHeader(String name) {
-                return false;  //To change body of implemented methods use File | Settings | File Templates.
+                return false;  
             }
 
             public String encodeURL(String url) {
-                return null;  //To change body of implemented methods use File | Settings | File Templates.
+                return null;  
             }
 
             public String encodeRedirectURL(String url) {
-                return null;  //To change body of implemented methods use File | Settings | File Templates.
+                return null;  
             }
 
             public String encodeUrl(String url) {
-                return null;  //To change body of implemented methods use File | Settings | File Templates.
+                return null;  
             }
 
             public String encodeRedirectUrl(String url) {
-                return null;  //To change body of implemented methods use File | Settings | File Templates.
+                return null;  
             }
 
             public void sendError(int sc, String msg) throws IOException {
-                //To change body of implemented methods use File | Settings | File Templates.
+                
             }
 
             public void sendError(int sc) throws IOException {
-                //To change body of implemented methods use File | Settings | File Templates.
+                
             }
 
             public void sendRedirect(String location) throws IOException {
-                //To change body of implemented methods use File | Settings | File Templates.
+                
             }
 
             public void setDateHeader(String name, long date) {
-                //To change body of implemented methods use File | Settings | File Templates.
+                
             }
 
             public void addDateHeader(String name, long date) {
-                //To change body of implemented methods use File | Settings | File Templates.
+                
             }
 
             public void setHeader(String name, String value) {
-                //To change body of implemented methods use File | Settings | File Templates.
+                
             }
 
             public void addHeader(String name, String value) {
-                //To change body of implemented methods use File | Settings | File Templates.
+                
             }
 
             public void setIntHeader(String name, int value) {
-                //To change body of implemented methods use File | Settings | File Templates.
+                
             }
 
             public void addIntHeader(String name, int value) {
-                //To change body of implemented methods use File | Settings | File Templates.
+                
             }
 
             public void setStatus(int sc) {
-                //To change body of implemented methods use File | Settings | File Templates.
+                
             }
 
             public void setStatus(int sc, String sm) {
-                //To change body of implemented methods use File | Settings | File Templates.
+                
             }
 
             public int getStatus() {
-                return 0;  //To change body of implemented methods use File | Settings | File Templates.
+                return 0;  
             }
 
             public String getHeader(String name) {
-                return null;  //To change body of implemented methods use File | Settings | File Templates.
+                return null;  
             }
 
             public Collection<String> getHeaders(String name) {
-                return null;  //To change body of implemented methods use File | Settings | File Templates.
+                return null;  
             }
 
             public Collection<String> getHeaderNames() {
-                return null;  //To change body of implemented methods use File | Settings | File Templates.
+                return null;  
             }
 
             public String getCharacterEncoding() {
-                return null;  //To change body of implemented methods use File | Settings | File Templates.
+                return null;  
             }
 
             public String getContentType() {
-                return null;  //To change body of implemented methods use File | Settings | File Templates.
+                return null;  
             }
 
             public ServletOutputStream getOutputStream() throws IOException {
-                return null;  //To change body of implemented methods use File | Settings | File Templates.
+                return null;  
             }
 
             public PrintWriter getWriter() throws IOException {
-                return null;  //To change body of implemented methods use File | Settings | File Templates.
+                return null;  
             }
 
             public void setCharacterEncoding(String charset) {
-                //To change body of implemented methods use File | Settings | File Templates.
+                
             }
 
             public void setContentLength(int len) {
-                //To change body of implemented methods use File | Settings | File Templates.
+                
             }
 
             public void setContentType(String type) {
-                //To change body of implemented methods use File | Settings | File Templates.
+                
             }
 
             public void setBufferSize(int size) {
-                //To change body of implemented methods use File | Settings | File Templates.
+                
             }
 
             public int getBufferSize() {
-                return 0;  //To change body of implemented methods use File | Settings | File Templates.
+                return 0;  
             }
 
             public void flushBuffer() throws IOException {
-                //To change body of implemented methods use File | Settings | File Templates.
+                
             }
 
             public void resetBuffer() {
-                //To change body of implemented methods use File | Settings | File Templates.
+                
             }
 
             public boolean isCommitted() {
-                return false;  //To change body of implemented methods use File | Settings | File Templates.
+                return false;  
             }
 
             public void reset() {
-                //To change body of implemented methods use File | Settings | File Templates.
+                
             }
 
             public void setLocale(Locale loc) {
-                //To change body of implemented methods use File | Settings | File Templates.
+                
             }
 
             public Locale getLocale() {
-                return null;  //To change body of implemented methods use File | Settings | File Templates.
+                return null;  
             }
         });
         this.webSocketSupport = webSocketSupport;
