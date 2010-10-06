2c1aa69730f0dce2a9904fdf84582bbe7ddaaea6
jfarcand <jfarcand@apache.org>
2010-10-06 18:14:20 -0400

    Fix for https://atmosphere.dev.java.net/issues/show_bug.cgi?id=68 ("Add a Wicket sample")
    Also removed the Jersey sample as it was originally there only for testing purpose.

---
+++ samples/wicket-clock/src/main/java/org/atmosphere/samples/wicket/WicketPushApplication.java
@@ -0,0 +1,81 @@
+/*
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 2007-2008 Sun Microsystems, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License. You can obtain
+ * a copy of the License at https://glassfish.dev.java.net/public/CDDL+GPL.html
+ * or glassfish/bootstrap/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at glassfish/bootstrap/legal/LICENSE.txt.
+ * Sun designates this particular file as subject to the "Classpath" exception
+ * as provided by Sun in the GPL Version 2 section of the License file that
+ * accompanied this code.  If applicable, add the following below the License
+ * Header, with the fields enclosed by brackets [] replaced by your own
+ * identifying information: "Portions Copyrighted [year]
+ * [name of copyright owner]"
+ *
+ * Contributor(s):
+ *
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ *
+ */
+package org.atmosphere.samples.wicket;
+
+import org.apache.wicket.protocol.http.WebApplication;
+import org.atmosphere.cpr.Broadcaster;
+import org.atmosphere.cpr.BroadcasterFactory;
+import org.atmosphere.cpr.DefaultBroadcaster;
+
+import java.util.Date;
+import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Create a {@link Broadcaster} that will broadcast the {@link Date} every second to the set of suspended Wicket connections.
+ *
+ * @author Andrey Belyaev
+ * @author Jeanfrancois Arcand
+ */
+public class WicketPushApplication extends WebApplication {
+    private final Broadcaster broadcaster;
+
+    /**
+     * Constructor
+     */
+    public WicketPushApplication() throws InstantiationException, IllegalAccessException {
+        broadcaster = BroadcasterFactory.getDefault().get(DefaultBroadcaster.class, "Wicket");
+        broadcaster.scheduleFixedBroadcast(new Callable<String>() {
+            public String call() {
+                String s = new Date().toString();
+                return s;
+            }
+        }, 1, TimeUnit.SECONDS);
+    }
+
+    /**
+     * @see org.apache.wicket.Application#getHomePage()
+     */
+    public Class<HomePage> getHomePage() {
+        return HomePage.class;
+    }
+
+    public Broadcaster getBroadcaster() {
+        return broadcaster;
+    }
+}
