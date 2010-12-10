6111827c785c8a1b41099b9402146210e32976bc
jfarcand <jfarcand@apache.org>
2010-12-10 10:30:39 -0500

    Another try

--- modules/jquery/src/main/webapp/jquery/jquery.atmosphere.js
+++ modules/jquery/src/main/webapp/jquery/jquery.atmosphere.js
@@ -371,9 +371,8 @@ jQuery.atmosphere = function()
             var callback = jQuery.atmosphere.request.callback;
 
             if (url.indexOf("http") == -1 && url.indexOf("ws") == -1) {
-                jQuery.getScript('jquery/jquery.urlParser-1.0.0.js', function() {
-                    url = jQuery.urlParser.setBaseUrl(document.location).parse(url).assemble();
-                });
+                jQuery.getScript('jquery/jquery.urlParser-1.0.0.js');
+                url = jQuery.urlParser.setBaseUrl(document.location).parse(url).assemble();
             }
             var location = url.replace('http:', 'ws:').replace('https:', 'wss:');
 
