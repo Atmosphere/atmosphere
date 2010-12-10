0758e2311ac0e8022d5f4720272e31e365bbedc1
jfarcand <jfarcand@apache.org>
2010-12-10 08:56:55 -0500

    Better handling of relative path with WebSocket

--- modules/jquery/src/main/webapp/jquery/jquery.atmosphere.js
+++ modules/jquery/src/main/webapp/jquery/jquery.atmosphere.js
@@ -371,7 +371,7 @@ jQuery.atmosphere = function()
             var callback = jQuery.atmosphere.request.callback;
 
             if (url.indexOf("http") == -1 && url.indexOf("ws") == -1) {
-                url = jQuery.atmosphere.canonicalize("") + url;
+                url = jQuery.atmosphere.parseUrl(url);
             }
             var location = url.replace('http:', 'ws:').replace('https:', 'wss:');
 
@@ -588,13 +588,28 @@ jQuery.atmosphere = function()
         }
         ,
 
-        canonicalize: function(url)
+        parseUrl: function(url)
         {
-            var div = document.createElement('div');
-            div.innerHTML = "<a></a>";
-            div.firstChild.href = url;
-            div.innerHTML = div.innerHTML;
-            return div.firstChild.href;
+            var count = 0;
+            var newurl = url
+            while (newurl.indexOf("../") != -1) {
+                newurl = url.substring(3, url.length);
+                count += 1;
+            }
+
+            var basePath = document.location.toString();
+
+            if (basePath.lastIndexOf("/") == basePath.length) {
+                basePath += basePath.substring(basePath.length - 1);
+            }
+
+            for (i =0; i < count; i++) {
+                if (basePath.lastIndexOf("/") != -1) {
+                    basePath = basePath.substring(0, basePath.lastIndexOf("/"));
+                }
+            }
+
+            return basePath + "/" + newurl;
         },
 
         close : function()
