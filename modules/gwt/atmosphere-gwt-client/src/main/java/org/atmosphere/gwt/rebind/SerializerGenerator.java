3d62e5f3a946d5cec2c18cebab0f4bb67d234b98
pierreh <java.net@jabbah.co.cc>
2011-06-01 13:10:20 +0200

    Updated GWT to version 2.3.0 (gwt-maven-plugin:2.3.0-SNAPSHOT must be used for the moment)

--- modules/gwt/atmosphere-gwt-client/src/main/java/org/atmosphere/gwt/rebind/SerializerGenerator.java
+++ modules/gwt/atmosphere-gwt-client/src/main/java/org/atmosphere/gwt/rebind/SerializerGenerator.java
@@ -104,8 +104,10 @@ public class SerializerGenerator extends GeneratorExt {
 					throw new UnableToCompleteException();
 				}
 				
-				SerializableTypeOracleBuilder typesSentToBrowserBuilder = new SerializableTypeOracleBuilder(logger, context.getPropertyOracle(), typeOracle);
-				SerializableTypeOracleBuilder typesSentFromBrowserBuilder = new SerializableTypeOracleBuilder(logger, context.getPropertyOracle(), typeOracle);
+				SerializableTypeOracleBuilder typesSentToBrowserBuilder = new SerializableTypeOracleBuilder(
+                        logger, context.getPropertyOracle(), context);
+				SerializableTypeOracleBuilder typesSentFromBrowserBuilder = new SerializableTypeOracleBuilder(
+                        logger, context.getPropertyOracle(), context);
 				
 				for (Class<? extends Serializable> serializable : annotation.value()) {
 					int rank = 0;
