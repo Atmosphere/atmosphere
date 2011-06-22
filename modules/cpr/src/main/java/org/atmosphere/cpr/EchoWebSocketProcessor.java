4e479988f4f0a8b5cfd3e11d3a92f66fc43d2655
jfarcand <jfarcand@apache.org>
2011-06-22 15:30:14 -0400

    Fix for http://java.net/jira/browse/ATMOSPHERE-131 ("Inconsistent transport implementation")
    Added support for custom WebSocketProcessor. The default is still echo, e.g when a websocket message arrive, the message gets broadcasted to all suspended connection. There is a new one called HttpServletRequestWebSocketProcessor that will invoke AtmosphereHandler#onRequest for all arriving message, so an application can control how the message gets broadcasted.

---
+++ modules/cpr/src/main/java/org/atmosphere/cpr/EchoWebSocketProcessor.java
@@ -0,0 +1,23 @@
+package org.atmosphere.cpr;
+
+import org.atmosphere.cpr.AtmosphereServlet;
+import org.atmosphere.cpr.WebSocketProcessor;
+import org.atmosphere.websocket.WebSocketSupport;
+
+
+public class EchoWebSocketProcessor extends WebSocketProcessor{
+
+    public EchoWebSocketProcessor(AtmosphereServlet atmosphereServlet, WebSocketSupport webSocketSupport) {
+        super(atmosphereServlet, webSocketSupport);
+    }
+
+    public void broadcast(String data) {
+        resource().getBroadcaster().broadcast(data);
+    }
+
+    public void broadcast(byte[] data, int offset, int length) {
+        byte[] b = new byte[length];
+        System.arraycopy(data, offset, b, 0, length);
+        resource().getBroadcaster().broadcast(b);
+    }
+}
