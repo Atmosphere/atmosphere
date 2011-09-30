33b9cb8077efd0120185f242c56bfca008a4b0f6
jfarcand <jfarcand@apache.org>
2011-09-30 08:35:34 -0400

    If Atmosphere gets used when Jetty is embedded, make sure the two way for creating Websocket still works

--- modules/cpr/src/main/java/org/atmosphere/container/Servlet30CometSupportWithWebSocket.java
+++ modules/cpr/src/main/java/org/atmosphere/container/Servlet30CometSupportWithWebSocket.java
@@ -120,10 +120,9 @@ public class Servlet30CometSupportWithWebSocket extends Servlet30CometSupport {
         if (!webSocketEnabled) {
             return super.service(req, res);
         } else {
-            if (webSocketFactory != null && req.getAttribute("websocket") == null) {
-                req.setAttribute("websocket", "inprocess");
+            if (webSocketFactory != null && !(Boolean)req.getAttribute(WebSocketSupport.WEBSOCKET_INITIATED)) {
                 webSocketFactory.acceptWebSocket(req, res);
-                req.setAttribute(WebSocketSupport.WEBSOCKET_SUSPEND, "true");
+                req.setAttribute(WebSocketSupport.WEBSOCKET_INITIATED, "true");
                 return new Action();
             }
 
