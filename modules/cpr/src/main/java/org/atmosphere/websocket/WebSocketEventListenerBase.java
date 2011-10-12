a4eece2ad1d600dbc363d4863fdfccc704cb6eb1
jfarcand <jfarcand@apache.org>
2011-10-12 11:00:37 -0400

    Add a base class for WebSocket listener

---
+++ modules/cpr/src/main/java/org/atmosphere/websocket/WebSocketEventListenerBase.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2011 Jeanfrancois Arcand
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.atmosphere.websocket;
+
+import org.atmosphere.cpr.AtmosphereResourceEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class WebSocketEventListenerBase implements WebSocketEventListener {
+
+    private final static Logger logger = LoggerFactory.getLogger(WebSocketEventListenerBase.class);
+
+    @Override
+    public void onHandshake(WebSocketEvent event) {
+        logger.trace("",event);
+    }
+
+    @Override
+    public void onMessage(WebSocketEvent event) {
+        logger.trace("",event);
+    }
+
+    @Override
+    public void onClose(WebSocketEvent event) {
+        logger.trace("",event);
+    }
+
+    @Override
+    public void onControl(WebSocketEvent event) {
+        logger.trace("",event);
+    }
+
+    @Override
+    public void onDisconnect(WebSocketEvent event) {
+        logger.trace("",event);
+    }
+
+    @Override
+    public void onConnect(WebSocketEvent event) {
+        logger.trace("",event);
+    }
+
+    @Override
+    public void onSuspend(AtmosphereResourceEvent<HttpServletRequest, HttpServletResponse> event) {
+        logger.trace("",event);
+    }
+
+    @Override
+    public void onResume(AtmosphereResourceEvent<HttpServletRequest, HttpServletResponse> event) {
+        logger.trace("",event);
+    }
+
+    @Override
+    public void onDisconnect(AtmosphereResourceEvent<HttpServletRequest, HttpServletResponse> event) {
+        logger.trace("",event);
+    }
+
+    @Override
+    public void onBroadcast(AtmosphereResourceEvent<HttpServletRequest, HttpServletResponse> event) {
+        logger.trace("",event);
+    }
+
+    @Override
+    public void onThrowable(AtmosphereResourceEvent<HttpServletRequest, HttpServletResponse> event) {
+        logger.trace("",event);
+    }
+}
