2024d0db8496736d697c294a016c9c007af906a9
jfarcand <jfarcand@apache.org>
2011-10-24 11:37:58 -0400

    Add a concurrent resource test

---
+++ modules/jersey/src/test/java/org/atmosphere/jersey/tests/ConcurrentResourceTest.java
@@ -0,0 +1,107 @@
+/*
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License. You can obtain
+ * a copy of the License at https://jersey.dev.java.net/CDDL+GPL.html
+ * or jersey/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at jersey/legal/LICENSE.txt.
+ * Sun designates this particular file as subject to the "Classpath" exception
+ * as provided by Sun in the GPL Version 2 section of the License file that
+ * accompanied this code.  If applicable, add the following below the License
+ * Header, with the fields enclosed by brackets [] replaced by your own
+ * identifying information: "Portions Copyrighted [year]
+ * [name of copyright owner]"
+ *
+ * Contributor(s):
+ *
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
+
+package org.atmosphere.jersey.tests;
+
+import com.ning.http.client.AsyncCompletionHandler;
+import com.ning.http.client.AsyncHttpClient;
+import com.ning.http.client.Response;
+import org.atmosphere.cpr.Broadcaster;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.fail;
+
+public class ConcurrentResourceTest extends BaseGrizzyTest {
+    private static final Logger logger = LoggerFactory.getLogger(ConcurrentResourceTest.class);
+
+    private static final int MAX_CLIENT = 100;
+
+    private final static CountDownLatch suspended = new CountDownLatch(MAX_CLIENT);
+
+    String getUrlTarget(int port) {
+        return "http://127.0.0.1:" + port + "/concurrent";
+    }
+
+    @Test(timeOut = 60000, enabled = true)
+    public void testConcurrentSuspendAndBroadcast() {
+        logger.info("Running testConcurrentSuspendAndBroadcast");
+
+        AsyncHttpClient c = new AsyncHttpClient();
+        Broadcaster b = null;
+        try {
+            final AtomicReference<StringBuffer> r = new AtomicReference<StringBuffer>(new StringBuffer());
+            for (int i = 0; i < MAX_CLIENT; i++) {
+
+                c.prepareGet(urlTarget).execute(new AsyncCompletionHandler<Response>() {
+
+                    @Override
+                    public Response onCompleted(Response response) throws Exception {
+                        r.get().append(response.getResponseBody());
+                        suspended.countDown();
+                        logger.info("suspendedCount" + suspended.getCount());
+                        return response;
+                    }
+                });
+            }
+
+            suspended.await(60, TimeUnit.SECONDS);
+
+            StringBuffer b2 = new StringBuffer();
+            for (int i = 0; i < MAX_CLIENT; i++) {
+                b2.append("foo");
+            }
+
+            assertEquals(r.get().toString(), b2.toString());
+
+        } catch (Exception e) {
+            logger.error("test failed", e);
+            fail(e.getMessage());
+        } finally {
+            if (b != null) b.destroy();
+        }
+        c.close();
+    }
+
+
+}
