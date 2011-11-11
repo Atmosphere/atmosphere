895e5cea1513c9d12a283fe41f1d2c4d2097b4a7
jfarcand <jfarcand@apache.org>
2011-11-11 11:54:04 -0500

    Fix merge clash

--- modules/cpr/src/main/java/org/atmosphere/cpr/AsynchronousProcessor.java
+++ modules/cpr/src/main/java/org/atmosphere/cpr/AsynchronousProcessor.java
@@ -41,6 +41,7 @@ package org.atmosphere.cpr;
 import org.atmosphere.cpr.AtmosphereServlet.Action;
 import org.atmosphere.cpr.AtmosphereServlet.AtmosphereConfig;
 import org.atmosphere.cpr.AtmosphereServlet.AtmosphereHandlerWrapper;
+import org.atmosphere.util.uri.UriTemplate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -51,6 +52,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -228,6 +231,23 @@ public abstract class AsynchronousProcessor implements CometSupport<AtmosphereRe
         aliveRequests.remove(r.getRequest());
     }
 
+    protected AtmosphereHandlerWrapper map(String path) {
+        AtmosphereHandlerWrapper atmosphereHandlerWrapper = config.handlers().get(path);
+        if (atmosphereHandlerWrapper == null) {
+            final Map<String, String> m = new HashMap<String, String>();
+            for (Map.Entry<String, AtmosphereHandlerWrapper> e : config.handlers().entrySet()) {
+                UriTemplate t = new UriTemplate(e.getKey());
+                logger.trace("Trying to map {} to {}", t, path);
+                if (t.match(path, m)) {
+                    atmosphereHandlerWrapper = e.getValue();
+                    logger.trace("Mapped {} to {}", t, e.getValue());
+                    break;
+                }
+            }
+        }
+        return atmosphereHandlerWrapper;
+    }
+
     /**
      * Return the {@link AtmosphereHandler} mapped to the passed servlet-path.
      *
@@ -236,49 +256,18 @@ public abstract class AsynchronousProcessor implements CometSupport<AtmosphereRe
      * @throws javax.servlet.ServletException
      */
     protected AtmosphereHandlerWrapper map(HttpServletRequest req) throws ServletException {
-        String path = req.getServletPath();
-        if (path == null || path.length() == 0) {
-            path = "/";
+        String path = req.getServletPath() + req.getPathInfo();
+        if (path == null || path.length() <= 1) {
+            path = "/all";
         }
 
-        AtmosphereHandlerWrapper atmosphereHandlerWrapper = config.handlers().get(path);
+        AtmosphereHandlerWrapper atmosphereHandlerWrapper = map(path);
         if (atmosphereHandlerWrapper == null) {
-            // Try the /*
-            if (!path.endsWith("/")) {
-                path += "/*";
-            } else {
-                path += "*";
-            }
-            atmosphereHandlerWrapper = config.handlers().get(path);
-            if (atmosphereHandlerWrapper == null) {
-                atmosphereHandlerWrapper = config.handlers().get("/*");
-                if (atmosphereHandlerWrapper == null) {
+            atmosphereHandlerWrapper = map("/all");
+        }
 
-                    if (req.getPathInfo() != null) {
-                        // Try appending the pathInfo
-                        path = req.getServletPath() + req.getPathInfo();
-                    }
-
-                    atmosphereHandlerWrapper = config.handlers().get(path);
-                    if (atmosphereHandlerWrapper == null) {
-                        // Last chance
-                        if (!path.endsWith("/")) {
-                            path += "/*";
-                        } else {
-                            path += "*";
-                        }
-                        // Try appending the pathInfo
-                        atmosphereHandlerWrapper = config.handlers().get(path);
-                        if (atmosphereHandlerWrapper == null) {
-                            logger.warn("No AtmosphereHandler maps request for {}", path);
-                            for (String m : config.handlers().keySet()) {
-                                logger.warn("\tAtmosphereHandler registered: {}", m);
-                            }
-                            throw new ServletException("No AtmosphereHandler maps request for " + path);
-                        }
-                    }
-                }
-            }
+        if (atmosphereHandlerWrapper == null) {
+            throw new ServletException("No AtmosphereHandler maps request for " + path);
         }
         config.getBroadcasterFactory().add(atmosphereHandlerWrapper.broadcaster,
                 atmosphereHandlerWrapper.broadcaster.getID());
@@ -368,15 +357,14 @@ public abstract class AsynchronousProcessor implements CometSupport<AtmosphereRe
                 AtmosphereHandler<HttpServletRequest, HttpServletResponse> atmosphereHandler =
                         (AtmosphereHandler<HttpServletRequest, HttpServletResponse>)
                                 req.getAttribute(FrameworkConfig.ATMOSPHERE_HANDLER);
-
                 synchronized (r) {
                     atmosphereHandler.onStateChange(r.getAtmosphereResourceEvent());
                     r.setIsInScope(false);
+                }
 
-                    Meteor m = (Meteor) req.getAttribute(AtmosphereResourceImpl.METEOR);
-                    if (m != null) {
-                        m.destroy();
-                    }
+                Meteor m = (Meteor) req.getAttribute(AtmosphereResourceImpl.METEOR);
+                if (m != null) {
+                    m.destroy();
                 }
             } else {
                 r.getResponse().flushBuffer();
@@ -429,10 +417,9 @@ public abstract class AsynchronousProcessor implements CometSupport<AtmosphereRe
             // The closedDetector closed the connection.
             return timedoutAction;
         }
-
-        logger.debug("Cancelling the connection for request {}", req);
-
         req.setAttribute(MAX_INACTIVE, (long) -1);
+
+        logger.debug("Cancelling out the connection for request {}", req);
 
         try {
             re = (AtmosphereResourceImpl) req.getAttribute(FrameworkConfig.ATMOSPHERE_RESOURCE);
