777715d6fc2a2638e8bce66d63b1f148ba77a9b7
jfarcand <jfarcand@apache.org>
2011-11-19 16:23:15 +0100

    Fix dtupid mistake

--- modules/jquery/src/main/webapp/jquery/jquery.atmosphere.js
+++ modules/jquery/src/main/webapp/jquery/jquery.atmosphere.js
@@ -747,7 +747,7 @@ jQuery.atmosphere = function() {
 
                     jQuery.atmosphere.request = request;
                     jQuery.atmosphere.executeRequest();
-                } else if (jQuery.atmosphere.subscribed && jQuery.atmosphere.response == 'websocket') {
+                } else if (jQuery.atmosphere.subscribed && jQuery.atmosphere.response.transport == 'websocket') {
                     jQuery.atmosphere.invokeCallback(jQuery.atmosphere.response);
 
                     if (request.requestCount++ < request.maxRequest) {
