1fe2078aa9ae25fdea99f26240adba9f49c5eef8
jfarcand <jfarcand@apache.org>
2011-11-24 13:44:43 -0500

    Remove that sample

--- samples/scala-longpolling-chat/src/main/scala/org/atmosphere/samples/lpchat/Message.scala
+++
@@ -1,8 +0,0 @@
-package org.atmosphere.samples.lpchat
-
-import scala.reflect.BeanProperty;
-import java.util.Date;
-
-class Message(@BeanProperty val date : Date,
-              @BeanProperty val message : String) {
-}
