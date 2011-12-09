080b353b73f90686e18975394e251736029ecec3
jfarcand <jfarcand@apache.org>
2011-12-09 11:47:25 -0800

    Improve sample

--- samples/async-channel/src/main/java/org/atmosphere/samples/pubsub/AsynchronousAnnotation.java
+++ samples/async-channel/src/main/java/org/atmosphere/samples/pubsub/AsynchronousAnnotation.java
@@ -46,21 +46,31 @@ import javax.ws.rs.Produces;
 import java.util.concurrent.Callable;
 
 /**
- * Simple PubSub resource that demonstrate many functionality supported by
- * Atmosphere JQuery Plugin and Atmosphere Jersey extension.
+ * Simple Asynchronous sample that demonstrate how task can be asynchronously executed. This class support all protocol
+ * and use the {@link org.atmosphere.cpr.HeaderConfig#X_ATMOSPHERE_TRACKING_ID} to create a single {@link org.atmosphere.cpr.Broadcaster}
+ * associated with the remote client.
  *
  * @author Jeanfrancois Arcand
  */
 @Path("/async")
 @Produces("text/html;charset=ISO-8859-1")
 public class AsynchronousAnnotation {
-
+    /**
+     * Suspend the connection, wait for a broadcast events.
+     * @return
+     */
     @GET
     @Asynchronous
     public String subscribe() {
         return null;
     }
 
+    /**
+     * Asynchronously return a String.
+     * @param message a message sent by the client.
+     * @return a Callable that will resume the suspended connection if long-polling/jsonp is used, or keep-alive the
+     * connection for websocket and http streaming.
+     */
     @POST
     @Asynchronous
     public Callable<String> publish(final @FormParam("message") String message) {
