224bd2da352edc89ccd280cfc3074f9343f84740
jfarcand <jfarcand@apache.org>
2012-01-09 07:51:58 -0500

    Add licence header

--- modules/cpr/src/main/java/org/atmosphere/websocket/WebSocketHandshakeFilter.java
+++ modules/cpr/src/main/java/org/atmosphere/websocket/WebSocketHandshakeFilter.java
@@ -1,3 +1,18 @@
+/*
+* Copyright 2011 Jeanfrancois Arcand
+*
+* Licensed under the Apache License, Version 2.0 (the "License"); you may not
+* use this file except in compliance with the License. You may obtain a copy of
+* the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+* WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+* License for the specific language governing permissions and limitations under
+* the License.
+*/
 package org.atmosphere.websocket;
 
 import javax.servlet.Filter;
@@ -12,10 +27,13 @@ import java.io.IOException;
 
 import static org.atmosphere.cpr.HeaderConfig.X_ATMOSPHERE_ERROR;
 
+/**
+ * A Servlet Filter for configuring which WebSocket protocol version an application want to support.
+ */
 public class WebSocketHandshakeFilter implements Filter {
+
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-        // TODO: add configuration
     }
 
     @Override
