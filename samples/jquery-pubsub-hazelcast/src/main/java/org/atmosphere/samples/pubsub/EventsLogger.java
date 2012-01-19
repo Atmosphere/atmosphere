594b43cdc12c567231ab06cbfb1647e9ebc852d3
jfarcand <jfarcand@apache.org>
2012-01-19 16:40:03 -0500

    Fix for #159 Add support for Hazelcast

---
+++ samples/jquery-pubsub-hazelcast/src/main/java/org/atmosphere/samples/pubsub/EventsLogger.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2012 Jeanfrancois Arcand
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.atmosphere.samples.pubsub;
+
+import org.atmosphere.cpr.AtmosphereResourceEvent;
+import org.atmosphere.cpr.AtmosphereResourceEventListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class EventsLogger implements AtmosphereResourceEventListener {
+
+    private static final Logger logger = LoggerFactory.getLogger(EventsLogger.class);
+
+    public EventsLogger() {
+    }
+
+    public void onSuspend(final AtmosphereResourceEvent<HttpServletRequest, HttpServletResponse> event) {
+        logger.info("onSuspend(): {}:{}", event.getResource().getRequest().getRemoteAddr(),
+                event.getResource().getRequest().getRemotePort());
+    }
+
+    public void onResume(AtmosphereResourceEvent<HttpServletRequest, HttpServletResponse> event) {
+        logger.info("onResume(): {}:{}", event.getResource().getRequest().getRemoteAddr(),
+                event.getResource().getRequest().getRemotePort());
+    }
+
+    public void onDisconnect(AtmosphereResourceEvent<HttpServletRequest, HttpServletResponse> event) {
+        logger.info("onDisconnect(): {}:{}", event.getResource().getRequest().getRemoteAddr(),
+                event.getResource().getRequest().getRemotePort());
+    }
+
+    public void onBroadcast(AtmosphereResourceEvent<HttpServletRequest, HttpServletResponse> event) {
+        logger.info("onBroadcast(): {}", event.getMessage());
+    }
+
+    public void onThrowable(AtmosphereResourceEvent<HttpServletRequest, HttpServletResponse> event) {
+        logger.warn("onThrowable(): {}", event);
+    }
+}
