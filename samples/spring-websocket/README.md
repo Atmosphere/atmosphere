939d804e49a374be515b6a7e6b4c7d125523bcac
jfarcand <jfarcand@apache.org>
2012-02-09 16:39:48 -0500

    Add Spring-WebSocket-Meteor sample from Dan Vulpe

---
+++ samples/spring-websocket/README.md
@@ -0,0 +1,9 @@
+Atmosphere Publisher Subscriber WebSockets Example
+==========================================
+
+This is a web application showing simple integration between Atmosphere (https://github.com/Atmosphere/atmosphere) and Spring MVC 3.1
+It uses a delegating WebSocketProtocol to intercept websockets channels.
+
+Example has been tested against Firefox, Chrome and Safari.
+
+There is no WebSockets support for IE (yet).
---
+++ samples/spring-websocket/src/main/java/org/atmosphere/samples/pubsub/utils/AtmosphereUtils.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2012 Jeanfrancois Arcand
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+/**
+ * This code was donated by Dan Vulpe https://github.com/dvulpe/atmosphere-ws-pubsub
+ */
+package org.atmosphere.samples.pubsub.utils;
+
+import org.atmosphere.cpr.AtmosphereResource;
+import org.atmosphere.cpr.AtmosphereResourceEvent;
+import org.atmosphere.cpr.AtmosphereResourceEventListenerAdapter;
+import org.atmosphere.cpr.Meteor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.concurrent.CountDownLatch;
+
+public final class AtmosphereUtils {
+
+
+    public static final Logger LOG = LoggerFactory.getLogger(AtmosphereUtils.class);
+
+    public static AtmosphereResource<HttpServletRequest, HttpServletResponse> getAtmosphereResource(HttpServletRequest request) {
+        return getMeteor(request).getAtmosphereResource();
+    }
+
+    public static Meteor getMeteor(HttpServletRequest request) {
+        return Meteor.build(request);
+    }
+
+    public static void suspend(final AtmosphereResource<HttpServletRequest, HttpServletResponse> resource) {
+        final CountDownLatch countDownLatch = new CountDownLatch(1);
+        resource.addEventListener(new AtmosphereResourceEventListenerAdapter() {
+            @Override
+            public void onSuspend(AtmosphereResourceEvent<HttpServletRequest, HttpServletResponse> event) {
+                countDownLatch.countDown();
+                resource.removeEventListener(this);
+            }
+        });
+        resource.suspend();
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            LOG.error("Interrupted while trying to suspend resource {}", resource);
+        }
+    }
+}
