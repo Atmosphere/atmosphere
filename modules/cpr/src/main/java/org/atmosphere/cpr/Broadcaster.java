0b302cf93edee0e2ab3c9998f380ede8e8ea7727
jfarcand <jfarcand@apache.org>
2012-03-28 08:38:30 -0400

    Fix for #252 [runtime] Improve Broadcaster fluid API

--- modules/cpr/src/main/java/org/atmosphere/cpr/Broadcaster.java
+++ modules/cpr/src/main/java/org/atmosphere/cpr/Broadcaster.java
@@ -201,7 +201,7 @@ public interface Broadcaster extends Trackable {
      * @param resource an {@link AtmosphereResource}
      * @return {@link AtmosphereResource} if added, or null if it was already there.
      */
-    AtmosphereResource addAtmosphereResource(AtmosphereResource resource);
+    Broadcaster addAtmosphereResource(AtmosphereResource resource);
 
     /**
      * Remove a {@link AtmosphereResource} from the list of item to be notified when
@@ -210,7 +210,7 @@ public interface Broadcaster extends Trackable {
      * @param resource an {@link AtmosphereResource}
      * @return {@link AtmosphereResource} if removed, or null if it was not.
      */
-    AtmosphereResource removeAtmosphereResource(AtmosphereResource resource);
+    Broadcaster removeAtmosphereResource(AtmosphereResource resource);
 
     /**
      * Set the {@link BroadcasterConfig} instance.
--- modules/cpr/src/main/java/org/atmosphere/cpr/DefaultBroadcaster.java
+++ modules/cpr/src/main/java/org/atmosphere/cpr/DefaultBroadcaster.java
@@ -995,12 +995,12 @@ public class DefaultBroadcaster implements Broadcaster {
      * {@inheritDoc}
      */
     @Override
-    public AtmosphereResource addAtmosphereResource(AtmosphereResource r) {
+    public Broadcaster addAtmosphereResource(AtmosphereResource r) {
 
         try {
             if (destroyed.get()) {
                 logger.debug(DESTROYED, getID(), "addAtmosphereResource(AtmosphereResource<?, ?> r");
-                return r;
+                return this;
             }
 
             start();
@@ -1027,7 +1027,7 @@ public class DefaultBroadcaster implements Broadcaster {
             }
 
             if (resources.contains(r)) {
-                return r;
+                return this;
             }
 
             // We need to synchronize here to let the push method cache message.
@@ -1051,7 +1051,7 @@ public class DefaultBroadcaster implements Broadcaster {
                 }
             }
         }
-        return r;
+        return this;
     }
 
     private boolean isAtmosphereResourceValid(AtmosphereResource r) {
@@ -1064,15 +1064,15 @@ public class DefaultBroadcaster implements Broadcaster {
      * {@inheritDoc}
      */
     @Override
-    public AtmosphereResource removeAtmosphereResource(AtmosphereResource r) {
+    public Broadcaster removeAtmosphereResource(AtmosphereResource r) {
 
         if (destroyed.get()) {
             logger.debug(DESTROYED, getID(), "removeAtmosphereResource(AtmosphereResource r)");
-            return r;
+            return this;
         }
 
         if (!resources.contains(r)) {
-            return null;
+            return this;
         }
         // Prevent two thread to mix operation
         boolean removed = resources.remove(r);
@@ -1089,7 +1089,7 @@ public class DefaultBroadcaster implements Broadcaster {
                 }
             }
         }
-        return r;
+        return this;
     }
 
     private void notifyIdleListener() {
