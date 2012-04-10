ba7767007c2679368b5c246fbbfca4ed52714a3f
jfarcand <jfarcand@apache.org>
2012-04-10 11:57:36 -0400

    Fix for #272 [runtime] Deprecate Jetty 7.4.x and 8.0.0 Mx Websocket support         #271 [runtime] Deprecate WebLogic support.
    Remove dependencies on WebLogic and really old Jetty version, and instead make them extra module.

---
+++ extras/jetty-websocketdraft8/src/main/java/org/atmosphere/jetty/JettyWebSocketDraft08Handler.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright 2012 Jeanfrancois Arcand
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.atmosphere.jetty;
+
+import org.atmosphere.cpr.AtmosphereFramework;
+import org.atmosphere.cpr.AtmosphereRequest;
+import org.atmosphere.websocket.WebSocketEventListener;
+import org.atmosphere.websocket.WebSocketProcessor;
+import org.atmosphere.websocket.WebSocketProtocol;
+import org.eclipse.jetty.websocket.WebSocket;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.UnsupportedEncodingException;
+
+import static org.atmosphere.websocket.WebSocketEventListener.WebSocketEvent.TYPE.DISCONNECT;
+import static org.atmosphere.websocket.WebSocketEventListener.WebSocketEvent.TYPE.MESSAGE;
+
+/**
+ * Jetty 7 & 8 WebSocket support.
+ */
+public class JettyWebSocketDraft08Handler implements WebSocket {
+
+    private static final Logger logger = LoggerFactory.getLogger(JettyWebSocketDraft08Handler.class);
+
+    private WebSocketProcessor webSocketProcessor;
+    private final AtmosphereRequest request;
+    private final AtmosphereFramework framework;
+    private final WebSocketProtocol webSocketProtocol;
+
+    public JettyWebSocketDraft08Handler(AtmosphereRequest request, AtmosphereFramework framework, WebSocketProtocol webSocketProtocol) {
+        this.request = request;
+        this.framework = framework;
+        this.webSocketProtocol = webSocketProtocol;
+    }
+
+    @Override
+    public void onConnect(org.eclipse.jetty.websocket.WebSocket.Outbound outbound) {
+
+        logger.debug("WebSocket.onConnect (outbound)");
+        try {
+            webSocketProcessor = new WebSocketProcessor(framework, new JettyWebSocket(outbound), webSocketProtocol);
+            webSocketProcessor.dispatch(request);
+        } catch (Exception e) {
+            logger.warn("failed to connect to web socket", e);
+        }
+    }
+
+    @Override
+    public void onMessage(byte frame, String data) {
+        logger.trace("WebSocket.onMessage (frame/string)");
+        webSocketProcessor.invokeWebSocketProtocol(data);
+        webSocketProcessor.notifyListener(new WebSocketEventListener.WebSocketEvent(data, MESSAGE, webSocketProcessor.webSocket()));
+    }
+
+    @Override
+    public void onMessage(byte frame, byte[] data, int offset, int length) {
+        logger.trace("WebSocket.onMessage (frame)");
+        webSocketProcessor.invokeWebSocketProtocol(new String(data, offset, length));
+        try {
+            webSocketProcessor.notifyListener(new WebSocketEventListener.WebSocketEvent(new String(data, offset, length, "UTF-8"), MESSAGE, webSocketProcessor.webSocket()));
+        } catch (UnsupportedEncodingException e) {
+            logger.warn("UnsupportedEncodingException", e);
+
+        }
+    }
+
+    @Override
+    public void onFragment(boolean more, byte opcode, byte[] data, int offset, int length) {
+        logger.trace("WebSocket.onFragment");
+        webSocketProcessor.invokeWebSocketProtocol(new String(data, offset, length));
+        try {
+            webSocketProcessor.notifyListener(new WebSocketEventListener.WebSocketEvent(new String(data, offset, length, "UTF-8"), MESSAGE, webSocketProcessor.webSocket()));
+        } catch (UnsupportedEncodingException e) {
+            logger.warn("UnsupportedEncodingException", e);
+
+        }
+    }
+
+    @Override
+    public void onDisconnect() {
+        request.destroy();
+        logger.trace("WebSocket.onDisconnect");
+        webSocketProcessor.close(1000);
+        webSocketProcessor.notifyListener(new WebSocketEventListener.WebSocketEvent("", DISCONNECT, webSocketProcessor.webSocket()));
+    }
+
+}
--- extras/weblogic/src/main/java/weblogic/servlet/http/RequestResponseKey.java
+++ extras/weblogic/src/main/java/weblogic/servlet/http/RequestResponseKey.java
@@ -14,11 +14,11 @@
  * the License.
  */
 /*
- *
+ * 
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
- *
+ * 
  * Copyright 2007-2008 Sun Microsystems, Inc. All rights reserved.
- *
+ * 
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
  * and Distribution License("CDDL") (collectively, the "License").  You
@@ -26,7 +26,7 @@
  * a copy of the License at https://glassfish.dev.java.net/public/CDDL+GPL.html
  * or glassfish/bootstrap/legal/LICENSE.txt.  See the License for the specific
  * language governing permissions and limitations under the License.
- *
+ * 
  * When distributing the software, include this License Header Notice in each
  * file and include the License file at glassfish/bootstrap/legal/LICENSE.txt.
  * Sun designates this particular file as subject to the "Classpath" exception
@@ -35,9 +35,9 @@
  * Header, with the fields enclosed by brackets [] replaced by your own
  * identifying information: "Portions Copyrighted [year]
  * [name of copyright owner]"
- *
+ * 
  * Contributor(s):
- *
+ * 
  * If you wish your version of this file to be governed by only the CDDL or
  * only the GPL Version 2, indicate your decision by adding "[Contributor]
  * elects to include this software in this distribution under the [CDDL or GPL
@@ -48,88 +48,29 @@
  * and therefore, elected the GPL Version 2 license, then the option applies
  * only if the new code is made subject to such option by the copyright
  * holder.
+ *
  */
-package org.atmosphere.container.version;
 
-import org.atmosphere.websocket.WebSocketAdapter;
-import org.eclipse.jetty.websocket.WebSocket.Outbound;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+package weblogic.servlet.http;
 
-import java.io.IOException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
- * Jetty 7.1/2 & 8 < M3 WebSocket support.
+ * Fake Weblogic class to allow compilation of support for that web container.
  *
  * @author Jeanfrancois Arcand
  */
-public class JettyWebSocket extends WebSocketAdapter {
-
-    private static final Logger logger = LoggerFactory.getLogger(JettyWebSocket.class);
-    private final Outbound outbound;
-    private final byte frame = 0x00;
-
-    public JettyWebSocket(Outbound outbound) {
-        this.outbound = outbound;
+public class RequestResponseKey {
+    public HttpServletRequest getRequest() {
+        throw new UnsupportedOperationException("Please remove the atmosphere-compat-weblogic from your classpath");
     }
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void writeError(int errorCode, String message) throws IOException {
-        logger.debug("{} {}", errorCode, message);
+    public HttpServletResponse getResponse() {
+        throw new UnsupportedOperationException("Please remove the atmosphere-compat-weblogic from your classpath");
     }
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void redirect(String location) throws IOException {
-        logger.error("redirect not supported");
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void write(String data) throws IOException {
-        if (!outbound.isOpen()) throw new IOException("Connection remotely closed");
-        logger.trace("WebSocket.write()");
-        outbound.sendMessage(frame, data);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void write(byte[] data) throws IOException {
-        if (!outbound.isOpen()) throw new IOException("Connection remotely closed");
-        logger.trace("WebSocket.write()");
-        outbound.sendMessage(frame, data, 0, data.length);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void write(byte[] data, int offset, int length) throws IOException {
-        if (!outbound.isOpen()) throw new IOException("Connection remotely closed");
-        logger.trace("WebSocket.write()");
-        outbound.sendMessage(frame, data, offset, length);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void close() throws IOException {
-        outbound.disconnect();
-    }
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void flush() throws IOException {
+    public void setTimeout(int i) {
+        throw new UnsupportedOperationException("Please remove the atmosphere-compat-weblogic from your classpath");
     }
 }
