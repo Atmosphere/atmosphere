544b0ed025cdea88bec8a5dd4ff5462242442eca
jfarcand <jfarcand@apache.org>
2012-05-01 12:10:49 -0400

    Fix for #324 return a 501 instead of a 202 when WebSocket Handshake fail

--- modules/cpr/src/main/java/org/atmosphere/websocket/WebSocketHandshakeFilter.java
+++ modules/cpr/src/main/java/org/atmosphere/websocket/WebSocketHandshakeFilter.java
@@ -68,7 +68,7 @@ public class WebSocketHandshakeFilter implements Filter {
                         logger.error("Invalid WebSocket Specification {} with {} ",
                                 r.getHeader("Connection"), r.getIntHeader("Sec-WebSocket-Version"));
                         HttpServletResponse.class.cast(response).addHeader(X_ATMOSPHERE_ERROR, "Websocket protocol not supported");
-                        HttpServletResponse.class.cast(response).sendError(202, "Websocket protocol not supported");
+                        HttpServletResponse.class.cast(response).sendError(501, "Websocket protocol not supported");
                         return;
                     }
                 }
