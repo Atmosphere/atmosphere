df49557e92fb4bfd9dfa3498a3e03509883b03c4
jfarcand <jfarcand@apache.org>
2012-05-02 09:59:25 -0400

    Display the supported transport

--- samples/chat/src/main/webapp/jquery/application.js
+++ samples/chat/src/main/webapp/jquery/application.js
@@ -1,6 +1,7 @@
 $(function () {
     "use strict";
 
+    var detect = $('#detect');
     var content = $('#content');
     var input = $('#input');
     var status = $('#status');
@@ -8,11 +9,38 @@ $(function () {
     var author = null;
     var logged = false;
     var socket = $.atmosphere;
+
+    <!-- The following code is just here for demonstration purpose and not required -->
+    var transports = new Array();
+    transports[0] = "websocket";
+    transports[1] = "sse";
+    transports[2] = "jsonp";
+    transports[3] = "long-polling";
+    transports[4] = "streaming";
+
+    transports.forEach(function (transport) {
+        var socket = $.atmosphere;
+        var req = { url: document.location.toString() + 'chat',
+            contentType : "application/json",
+            logLevel : 'debug',
+            transport : transport ,
+            maxRequest : 0,
+            fallbackTransport : 'none',
+            headers : { "negotiating" : "true" }};
+
+        req.onOpen = function(response) {
+            detect.append('<p><span style="color:blue">' + transport + ' supported: '  + '</span>' + (response.transport == transport));
+        }
+
+        socket.subscribe(req)
+    });
+
+    // We are now ready to cut the request
     var request = { url: document.location.toString() + 'chat',
-                    contentType : "application/json",
-                    logLevel : 'debug',
-                    transport : 'websocket' ,
-                    fallbackTransport: 'long-polling'};
+        contentType : "application/json",
+        logLevel : 'debug',
+        transport : 'websocket' ,
+        fallbackTransport: 'long-polling'};
 
 
     request.onOpen = function(response) {
@@ -42,7 +70,7 @@ $(function () {
             input.removeAttr('disabled');
 
             var me = json.author == author;
-            var date =  typeof(json.time) == 'string' ? parseInt(json.time) : json.time;
+            var date = typeof(json.time) == 'string' ? parseInt(json.time) : json.time;
             addMessage(json.author, json.text, me ? 'blue' : 'black', new Date(date));
         }
     };
