66d5cfc06725047103afd3e0418217836d321b6a
Pierre Havelaar <github@jabbah.co.cc>
2012-05-22 16:55:33 +0200

    [GWT] #372 Add connection sharing between client windows

---
+++ extras/gwt/atmosphere-gwt-client/src/main/java/org/atmosphere/gwt/client/AtmosphereProxyEvent.java
@@ -0,0 +1,44 @@
+package org.atmosphere.gwt.client;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author p.havelaar
+ */
+public class AtmosphereProxyEvent implements Serializable {
+    public static enum EventType {
+        ON_CONNECTED, ON_BEFORE_DISCONNECTED, ON_DISCONNECTED, ON_ERROR, ON_HEARTBEAT, ON_REFRESH, ON_AFTER_REFRESH,
+        ON_MESSAGE, POST, BROADCAST,
+        ANNOUNCE_NEW_CHILD, ANNOUNCE_CHILD_DEATH, ELECT_MASTER, ADOPT_ORPHANS, ANNOUNCE_NEW_PARENT;
+    }
+    
+    protected EventType eventType = EventType.ON_ERROR;
+    protected transient Serializable data;
+    
+    public AtmosphereProxyEvent() {
+    }
+    
+    public AtmosphereProxyEvent(EventType type) {
+        this.eventType = type;
+    }
+
+    public AtmosphereProxyEvent setEventType(EventType eventType) {
+        this.eventType = eventType;
+        return this;
+    }
+
+    public EventType getEventType() {
+        return eventType;
+    }
+
+    public Serializable getData() {
+        return data;
+    }
+
+    public AtmosphereProxyEvent setData(Serializable data) {
+        this.data = data;
+        return this;
+    }
+
+} 
