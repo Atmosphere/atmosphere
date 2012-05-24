5bcae30028ec068001f8fe75a5a39a1070390871
Pierre Havelaar <github@jabbah.co.cc>
2012-05-24 08:50:04 +0200

    [GWT] add chatroom sample
    --HG--
    rename : extras/gwt/atmosphere-gwt-client/src/main/java/org/atmosphere/gwt/client/AtmosphereProxyEvent.java => extras/gwt/atmosphere-gwt-client/src/main/java/org/atmosphere/gwt/client/impl/AtmosphereProxyEvent.java

---
+++ samples/gwt-chat/src/main/java/org/atmosphere/samples/server/ChatHandler.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2012 Jeanfrancois Arcand
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.atmosphere.samples.server;
+
+import org.atmosphere.gwt.server.AtmosphereGwtHandler;
+import org.atmosphere.gwt.server.GwtAtmosphereResource;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.atmosphere.cpr.Broadcaster;
+import org.atmosphere.cpr.BroadcasterFactory;
+
+/**
+ * @author p.havelaar
+ */
+public class ChatHandler extends AtmosphereGwtHandler {
+
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+        super.init(servletConfig);
+        Logger.getLogger("").setLevel(Level.INFO);
+        Logger.getLogger("org.atmosphere.gwt").setLevel(Level.ALL);
+        Logger.getLogger("org.atmosphere.samples").setLevel(Level.ALL);
+        Logger.getLogger("").getHandlers()[0].setLevel(Level.ALL);
+        logger.trace("Updated logging levels");
+    }
+
+    @Override
+    public int doComet(GwtAtmosphereResource resource) throws ServletException, IOException {
+        String room = resource.getRequest().getPathInfo();
+        Broadcaster broadcaster = BroadcasterFactory.getDefault().lookup(room, true);
+        resource.getAtmosphereResource().setBroadcaster(broadcaster);
+        return 300000;
+    }
+
+    @Override
+    public void cometTerminated(GwtAtmosphereResource cometResponse, boolean serverInitiated) {
+        super.cometTerminated(cometResponse, serverInitiated);
+        logger.info("Comet disconnected");
+    }
+
+    @Override
+    public void doPost(HttpServletRequest postRequest, HttpServletResponse postResponse, List<Serializable> messages, GwtAtmosphereResource cometResource) {
+        broadcast(messages, cometResource);
+    }
+
+}
