45f6cf6f5e39db589ae1b5db8e446569ad2fb23c
jfarcand <jfarcand@apache.org>
2012-05-25 14:45:15 -0400

    Fic import

--- modules/cpr/src/main/java/org/atmosphere/cpr/AtmosphereRequest.java
+++ modules/cpr/src/main/java/org/atmosphere/cpr/AtmosphereRequest.java
@@ -39,7 +39,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.StringReader;
 import java.io.UnsupportedEncodingException;
-import java.lang.reflect.Array;
 import java.security.Principal;
 import java.util.ArrayList;
 import java.util.Arrays;
