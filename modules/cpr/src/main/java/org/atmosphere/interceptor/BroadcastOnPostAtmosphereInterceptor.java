0020f80900f4367041ba44ec177d679329093831
jfarcand <jfarcand@apache.org>
2012-06-07 13:23:32 -0400

    Fix for #403 [runtime] Add a BroadcastOnPost AtmosphereInterceptor

---
+++ modules/cpr/src/main/java/org/atmosphere/interceptor/BroadcastOnPostAtmosphereInterceptor.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2012 Jeanfrancois Arcand
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.atmosphere.interceptor;
+
+import org.atmosphere.cpr.Action;
+import org.atmosphere.cpr.AtmosphereConfig;
+import org.atmosphere.cpr.AtmosphereInterceptor;
+import org.atmosphere.cpr.AtmosphereResource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+/**
+ * This read the request's body and invoke the associated {@link org.atmosphere.cpr.Broadcaster} of an {@link AtmosphereResource}.
+ * The broadcast always happens AFTER the request has been delivered to an {@link AtmosphereHandler}
+ *
+ * @author Jeanfrancois Arcand
+ */
+public class BroadcastOnPostAtmosphereInterceptor implements AtmosphereInterceptor {
+
+    private final static Logger logger = LoggerFactory.getLogger(BroadcastOnPostAtmosphereInterceptor.class);
+
+    @Override
+    public void configure(AtmosphereConfig config) {
+    }
+
+    @Override
+    public Action inspect(AtmosphereResource r) {
+        return Action.CONTINUE;
+    }
+
+    @Override
+    public void postInspect(AtmosphereResource r) {
+        if (r.getRequest().getMethod().equalsIgnoreCase("POST")) {
+            StringBuilder stringBuilder = new StringBuilder();
+            BufferedReader bufferedReader = null;
+            try {
+                InputStream inputStream = r.getRequest().getInputStream();
+                if (inputStream != null) {
+                    bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                    char[] charBuffer = new char[128];
+                    int bytesRead = -1;
+                    while ((bytesRead = bufferedReader.read(charBuffer)) > 0) {
+                        stringBuilder.append(charBuffer, 0, bytesRead);
+                    }
+                } else {
+                    stringBuilder.append("");
+                }
+            } catch (IOException ex) {
+                logger.warn("", ex);
+            } finally {
+                if (bufferedReader != null) {
+                    try {
+                        bufferedReader.close();
+                    } catch (IOException ex) {
+                        logger.warn("", ex);
+                    }
+                }
+            }
+            r.getBroadcaster().broadcast(stringBuilder.toString());
+        }
+    }
+}
