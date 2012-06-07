4c07d3b1c06bdfd4e9e95f9699a1b94950660f14
jfarcand <jfarcand@apache.org>
2012-06-07 15:14:46 -0400

    Cosmetic

--- modules/cpr/src/main/java/org/atmosphere/cpr/AtmosphereRequest.java
+++ modules/cpr/src/main/java/org/atmosphere/cpr/AtmosphereRequest.java
@@ -16,7 +16,6 @@
 package org.atmosphere.cpr;
 
 import org.atmosphere.util.FakeHttpSession;
-import org.slf4j.LoggerFactory;
 
 import javax.servlet.AsyncContext;
 import javax.servlet.DispatcherType;
