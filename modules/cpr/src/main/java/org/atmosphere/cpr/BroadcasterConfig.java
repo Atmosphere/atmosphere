5d09ea392d4f152ce5a5f3af704ab0e1bb98e0ab
jfarcand <jfarcand@apache.org>
2012-06-07 12:52:36 -0400

    Fix typo

--- modules/cpr/src/main/java/org/atmosphere/cpr/BroadcasterConfig.java
+++ modules/cpr/src/main/java/org/atmosphere/cpr/BroadcasterConfig.java
@@ -147,7 +147,7 @@ public class BroadcasterConfig {
             }
 
             if (isExecutorShared && numberOfMessageProcessingThread == 1) {
-                logger.warn("Not enought numberOfMessageProcessingThread for a shareable thread pool {}, " +
+                logger.warn("Not enough numberOfMessageProcessingThread for a shareable thread pool {}, " +
                         "Setting it to a newCachedThreadPool", numberOfMessageProcessingThread);
                 numberOfMessageProcessingThread = -1;
             }
@@ -159,7 +159,7 @@ public class BroadcasterConfig {
             }
 
             if (isAsyncExecutorShared && numberOfAsyncThread == 1) {
-                logger.warn("Not enought numberOfAsyncThread for a shareable thread pool {}, " +
+                logger.warn("Not enough numberOfAsyncThread for a shareable thread pool {}, " +
                         "Setting it to a newCachedThreadPool", numberOfAsyncThread);
                 numberOfAsyncThread = -1;
             }
