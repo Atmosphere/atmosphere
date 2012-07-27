0c354e46c9a5bfe364c7767ee4f69b54bee1a603
jfarcand <jfarcand@apache.org>
2012-07-27 12:22:31 -0400

    Fix for #539 [atmosphere.js] Add a way to disable reading response's headers

--- samples/chat/src/main/webapp/jquery/application.js
+++ samples/chat/src/main/webapp/jquery/application.js
@@ -50,7 +50,7 @@ $(function () {
         contentType : "application/json",
         logLevel : 'debug',
         shared : 'true',
-        transport : transport ,
+        transport : 'long-polling' ,
         fallbackTransport: 'long-polling'};
 
 
