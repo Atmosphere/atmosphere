fa74202e73771b58437127066108c6bbc7a96767
jfarcand <jfarcand@apache.org>
2012-09-11 13:12:09 -0400

    Remove that library now that it has been officially released as it cause all kind of deploy issue

--- extras/gwt/gwtprojsonserializer/src/main/java/com/kfuntak/gwt/json/serialization/client/IncompatibleObjectException.java
+++
@@ -1,8 +0,0 @@
-package com.kfuntak.gwt.json.serialization.client;
-
-import com.google.gwt.json.client.JSONException;
-
-public class IncompatibleObjectException extends JSONException {
-
-    private static final long serialVersionUID = 1L;
-}
--- extras/gwt/gwtprojsonserializer/src/main/java/com/kfuntak/gwt/json/serialization/client/JsonSerializable.java
+++
@@ -1,4 +0,0 @@
-package com.kfuntak.gwt.json.serialization.client;
-
-public interface JsonSerializable {
-}
