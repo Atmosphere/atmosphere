9c09e6ef3b5beace16b51a18f06142f38138c09f
jfarcand <jfarcand@apache.org>
2012-09-18 09:54:19 -0400

    Incremental fix for #631 org.atmosphere.cpr.Meteor, cache Memory Leak

--- modules/cpr/src/main/java/org/atmosphere/cpr/AsynchronousProcessor.java
+++ modules/cpr/src/main/java/org/atmosphere/cpr/AsynchronousProcessor.java
@@ -493,14 +493,16 @@ public abstract class AsynchronousProcessor implements AsyncSupport<AtmosphereRe
                                 req.getAttribute(FrameworkConfig.ATMOSPHERE_HANDLER);
 
                 synchronized (r) {
-                    atmosphereHandler.onStateChange(r.getAtmosphereResourceEvent());
-
-                    Meteor m = (Meteor) req.getAttribute(AtmosphereResourceImpl.METEOR);
-                    if (m != null) {
-                        m.destroy();
+                    try {
+                        atmosphereHandler.onStateChange(r.getAtmosphereResourceEvent());
+                    } finally {
+                        Meteor m = (Meteor) req.getAttribute(AtmosphereResourceImpl.METEOR);
+                        if (m != null) {
+                            m.destroy();
+                        }
+                        req.removeAttribute(FrameworkConfig.ATMOSPHERE_RESOURCE);
                     }
                 }
-                req.removeAttribute(FrameworkConfig.ATMOSPHERE_RESOURCE);
             }
         } catch (IOException ex) {
             try {
