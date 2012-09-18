554d9a64dac9fdf4beff3c55497a927f99c5b7e6
jfarcand <jfarcand@apache.org>
2012-09-18 17:47:37 -0400

    Proper fix

--- modules/jquery/src/main/webapp/jquery/jquery.atmosphere.js
+++ modules/jquery/src/main/webapp/jquery/jquery.atmosphere.js
@@ -1526,7 +1526,7 @@ jQuery.atmosphere = function() {
                     _subscribed = true;
 
                 } else {
-                    if (_request.logLevel == 'debug') {
+                    if (rq.logLevel == 'debug') {
                         jQuery.atmosphere.log(rq.logLevel, ["Max re-connection reached."]);
                     }
                     _onError();
