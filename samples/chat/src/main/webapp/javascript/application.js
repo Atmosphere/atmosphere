dce5d86a33d693b66123fb6afcf5b5caa162c775
jfarcand <jfarcand@apache.org>
2012-12-13 11:56:45 -0500

    Migrate the chat to portal.js

--- samples/chat/src/main/webapp/javascript/application.js
+++ samples/chat/src/main/webapp/javascript/application.js
@@ -8,7 +8,7 @@ $(function () {
     var myName = false;
     var author = null;
     var logged = false;
-    var socket = $.atmosphere;
+    var socket = atmosphere;
     var subSocket;
     var transport = 'websocket';
 
@@ -35,7 +35,7 @@ $(function () {
 
     <!-- You can share messages between window/tabs.   -->
     request.onLocalMessage = function(message) {
-        if (transport != 'local') {
+        if (transport != 'session') {
             header.append($('<h4>', { text: 'A new tab/window has been opened'}).css('color', 'green'));
             if (myName) {
                 subSocket.pushLocal(myName);
@@ -52,7 +52,6 @@ $(function () {
 
     <!-- For demonstration of how you can customize the fallbackTransport using the onTransportFailure function -->
     request.onTransportFailure = function(errorMsg, request) {
-        jQuery.atmosphere.info(errorMsg);
         if (window.EventSource) {
             request.fallbackTransport = "sse";
         }
@@ -107,7 +106,7 @@ $(function () {
                 author = msg;
             }
 
-            subSocket.push(jQuery.stringifyJSON({ author: author, message: msg }));
+            subSocket.push(stringifyJSON({ author: author, message: msg }));
             $(this).val('');
 
             input.attr('disabled', 'disabled');
