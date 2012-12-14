713b691927138bcc70263c28e12239160371dd82
jfarcand <jfarcand@apache.org>
2012-12-14 13:25:19 -0500

    Never set the uuid first

--- modules/jquery/src/main/webapp/jquery/jquery.atmosphere.js
+++ modules/jquery/src/main/webapp/jquery/jquery.atmosphere.js
@@ -109,7 +109,7 @@ jQuery.atmosphere = function() {
                 connectTimeout : -1,
                 reconnectInterval : 0,
                 dropAtmosphereHeaders : true,
-                uuid : jQuery.atmosphere.guid(),
+                uuid : 0,
                 shared : false,
                 readResponsesHeaders : true,
                 maxReconnectOnClose: 5,
