3c255aec9924a8f3db9dfea419220edad10e4c1c
jfarcand <jfarcand@apache.org>
2012-12-14 11:27:42 -0500

    Fix for #777 Add Session's creation Interceptor

---
+++ modules/cpr/src/main/java/org/atmosphere/interceptor/SessionCreationInterceptor.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2012 Jeanfrancois Arcand
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.atmosphere.interceptor;
+
+import org.atmosphere.cpr.Action;
+import org.atmosphere.cpr.AtmosphereConfig;
+import org.atmosphere.cpr.AtmosphereInterceptor;
+import org.atmosphere.cpr.AtmosphereResource;
+
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+/**
+ * An interceptor that create a the {@link HttpSession} on the fist received request so transport like long-polling
+ * can have the share the session with request coming after the suspend operation. Without this and because
+ * with long-polling cookies aren't read by the browser until the response is resumed, the session id will not
+ * be the same so session couldn't be used.
+ *
+ * @author Jeanfrancois Arcand
+ */
+public class SessionCreationInterceptor implements AtmosphereInterceptor {
+
+    private ConcurrentLinkedQueue<String> ids = new ConcurrentLinkedQueue<String>();
+
+    @Override
+    public void configure(AtmosphereConfig config) {
+    }
+
+    @Override
+    public Action inspect(AtmosphereResource r) {
+
+        if (!ids.remove(r.uuid()) && r.getRequest().getMethod().equalsIgnoreCase("GET")) {
+            r.session(true);
+            ids.offer(r.uuid());
+            return Action.CANCELLED;
+        }
+        return Action.CONTINUE;
+    }
+
+    @Override
+    public void postInspect(AtmosphereResource r) {
+    }
+}
