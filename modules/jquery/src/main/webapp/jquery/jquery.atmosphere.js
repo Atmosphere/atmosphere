6adc147c4b5fcb8f2269988e2be9d3f6de11a578
jfarcand <jfarcand@apache.org>
2013-01-14 08:39:56 -0500

    Fix stupid typo which cause the script to fail

--- modules/jquery/src/main/webapp/jquery/jquery.atmosphere.js
+++ modules/jquery/src/main/webapp/jquery/jquery.atmosphere.js
@@ -2143,7 +2143,7 @@ jQuery.atmosphere = function() {
                     _localSocketF(_response.responseBody);
                 }
 
-                var messages = typeof((_response.responseBody) == 'string' && _request.trackMessageLength) ?
+                var messages = (typeof(_response.responseBody) == 'string' && _request.trackMessageLength) ?
                     _response.responseBody.split(_request.messageDelimiter) : new Array(_response.responseBody);
                 for (var i = 0; i < messages.length; i++) {
 
