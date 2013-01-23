14678063087fc4ee44c227828008c6d9281bf857
jfarcand <jfarcand@apache.org>
2013-01-23 11:41:18 +0100

    Incremental fix for #658

---
+++ modules/cpr/src/main/java/org/atmosphere/cache/EventCacheBroadcasterCache.java
@@ -0,0 +1,270 @@
+/*
+ * Copyright 2012 Jeanfrancois Arcand
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.atmosphere.cache;
+
+import org.atmosphere.cpr.ApplicationConfig;
+import org.atmosphere.cpr.AtmosphereResource;
+import org.atmosphere.cpr.AtmosphereResourceImpl;
+import org.atmosphere.cpr.Broadcaster;
+import org.atmosphere.cpr.BroadcasterCache;
+import org.atmosphere.cpr.BroadcasterFactory;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * An improved {@link BroadcasterCache} implementation.
+ *
+ * @author Paul Khodchenkov
+ * @author Jeanfrancois Arcand
+ */
+public class EventCacheBroadcasterCache implements BroadcasterCache {
+
+    private final Map<String, ClientQueue> messages = new HashMap<String, ClientQueue>();
+
+    private final Map<String, Long> activeClients = new HashMap<String, Long>();
+
+    private ScheduledFuture scheduledFuture;
+    protected ScheduledExecutorService taskScheduler = Executors.newSingleThreadScheduledExecutor();
+
+    private long clientIdleTime = TimeUnit.MINUTES.toMillis(2);//2 minutes
+
+    private long invalidateCacheInterval = TimeUnit.MINUTES.toMillis(1);//1 minute
+
+    public static class ClientQueue {
+
+        private List<CacheMessage> queue = new ArrayList<CacheMessage>();
+
+        private Set<String> ids = new HashSet<String>();
+
+        public List<CacheMessage> getQueue() {
+            return queue;
+        }
+
+        public Set<String> getIds() {
+            return ids;
+        }
+    }
+
+    public static class CacheMessage {
+
+        private final Object message;
+
+        private final String id;
+
+        private CacheMessage(String id, Object message) {
+            this.id = id;
+            this.message = message;
+        }
+
+        public Object getMessage() {
+            return message;
+        }
+
+        public String getId() {
+            return id;
+        }
+    }
+
+    public void setInvalidateCacheInterval(long invalidateCacheInterval) {
+        this.invalidateCacheInterval = invalidateCacheInterval;
+    }
+
+    public void setClientIdleTime(long clientIdleTime) {
+        this.clientIdleTime = clientIdleTime;
+    }
+
+    public void setExecutorService(ScheduledExecutorService reaper){
+        if (scheduledFuture != null) {
+            scheduledFuture.cancel(false);
+            scheduledFuture = null;
+        }
+
+        if (taskScheduler != null) {
+            stop();
+        }
+        this.taskScheduler = reaper;
+    }
+
+    @Override
+    public void start() {
+        scheduledFuture = taskScheduler.scheduleWithFixedDelay(new Runnable() {
+            @Override
+            public void run() {
+                invalidateExpiredEntries();
+            }
+        }, 0, invalidateCacheInterval, TimeUnit.MINUTES);
+    }
+
+    protected void invalidateExpiredEntries() {
+        long now = System.nanoTime();
+        synchronized (messages) {
+
+            Set<String> inactiveClients = new HashSet<String>();
+
+            for (Map.Entry<String, Long> entry : activeClients.entrySet()) {
+                if (now - entry.getValue() > clientIdleTime) {
+                    inactiveClients.add(entry.getKey());
+                }
+            }
+
+            for (String clientId : inactiveClients) {
+                activeClients.remove(clientId);
+                messages.remove(clientId);
+            }
+
+        }
+    }
+
+    @Override
+    public void stop() {
+        if (scheduledFuture != null) {
+            scheduledFuture.cancel(false);
+            scheduledFuture = null;
+        }
+        taskScheduler.shutdown();
+    }
+
+    @Override
+    public void addToCache(String broadcasterId, AtmosphereResource r, Object e) {
+        long now = System.nanoTime();
+        String messageId = UUID.randomUUID().toString();
+        CacheMessage cacheMessage = new CacheMessage(messageId, e);
+        synchronized (messages) {
+            if (r == null) {
+                //no clients are connected right now, caching message for all active clients
+                for (Map.Entry<String, Long> entry : activeClients.entrySet()) {
+                    addMessageIfNotExists(entry.getKey(), cacheMessage);
+                }
+            } else {
+                String clientId = r.transport() == AtmosphereResource.TRANSPORT.WEBSOCKET
+                        ? (String)r.getRequest().getAttribute(ApplicationConfig.SUSPENDED_ATMOSPHERE_RESOURCE_UUID) : r.uuid();
+
+                activeClients.put(clientId, now);
+                if (isAtmosphereResourceValid(r)) {//todo better to have cacheLost flag
+                    /**
+                     * This line is called for each AtmosphereResource (note that
+                     * broadcaster.getAtmosphereResources() may not return the current AtmosphereResource because
+                     * the resource may be destroyed by DefaultBroadcaster.executeAsyncWrite or JerseyBroadcasterUtil
+                     * concurrently, that is why we need to check duplicates),
+                     *
+                     * Cache the message only once for the clients
+                     * which are not currently connected to the server
+                     */
+                    Broadcaster broadcaster = getBroadCaster(broadcasterId);
+                    List<AtmosphereResource> resources = new ArrayList<AtmosphereResource>(broadcaster.getAtmosphereResources());
+                    Set<String> disconnectedClients = getDisconnectedClients(resources);
+                    for (String disconnectedId : disconnectedClients) {
+                        addMessageIfNotExists(disconnectedId, cacheMessage);
+                    }
+                } else {
+                    /**
+                     * Cache lost message, caching only for specific client.
+                     * Preventing duplicate inserts because this method can be called
+                     * concurrently from DefaultBroadcaster.executeAsyncWrite or JerseyBroadcasterUtil
+                     * when calling cacheLostMessage
+                     */
+                    addMessageIfNotExists(clientId, cacheMessage);
+                }
+            }
+        }
+    }
+
+    private boolean isAtmosphereResourceValid(AtmosphereResource r) {
+        return !r.isResumed()
+                && !r.isCancelled()
+                && AtmosphereResourceImpl.class.cast(r).isInScope();
+    }
+
+    private Set<String> getDisconnectedClients(List<AtmosphereResource> resources) {
+        Set<String> ids = new HashSet<String>(activeClients.keySet());
+        for (AtmosphereResource resource : resources) {
+            ids.remove(resource.uuid());
+        }
+        return ids;
+    }
+
+    private Broadcaster getBroadCaster(String broadcasterId) {
+        BroadcasterFactory factory = BroadcasterFactory.getDefault();
+        Broadcaster broadcaster = factory.lookup(broadcasterId, false);
+        return broadcaster;
+    }
+
+    private void  addMessageIfNotExists(String clientId, CacheMessage message) {
+        if (!hasMessage(clientId, message.getId())) {
+            addMessage(clientId, message);
+        }
+    }
+
+    private void addMessage(String clientId, CacheMessage message) {
+        ClientQueue clientQueue = messages.get(clientId);
+        if (clientQueue == null) {
+            clientQueue = new ClientQueue();
+            messages.put(clientId, clientQueue);
+        }
+        clientQueue.getQueue().add(message);
+        clientQueue.getIds().add(message.getId());
+    }
+
+    private boolean hasMessage(String clientId, String messageId) {
+        ClientQueue clientQueue = messages.get(clientId);
+        return clientQueue != null && clientQueue.getIds().contains(messageId);
+    }
+
+    @Override
+    public List<Object> retrieveFromCache(String broadcasterId, AtmosphereResource r) {
+        String clientId = r.uuid();
+        long now = System.nanoTime();
+
+        List<Object> result = new ArrayList<Object>();
+
+        ClientQueue clientQueue;
+        synchronized (messages) {
+            activeClients.put(clientId, now);
+            clientQueue = messages.remove(clientId);
+        }
+        List<CacheMessage> clientMessages;
+        if (clientQueue == null) {
+            clientMessages = Collections.emptyList();
+        } else {
+            clientMessages = clientQueue.getQueue();
+        }
+
+        for (CacheMessage cacheMessage : clientMessages) {
+            result.add(cacheMessage.getMessage());
+        }
+        return result;
+    }
+
+    public Map<String, ClientQueue> messages(){
+        return messages;
+    }
+
+    public  Map<String, Long> activeClients(){
+        return activeClients;
+    }
+
+}
---
+++ modules/cpr/src/test/java/org/atmosphere/cpr/EventCacheBroadcasterCacheTest.java
@@ -0,0 +1,152 @@
+/*
+ * Copyright 2012 Jean-Francois Arcand
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.atmosphere.cpr;
+
+import org.atmosphere.cache.EventCacheBroadcasterCache;
+import org.atmosphere.container.BlockingIOCometSupport;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import javax.servlet.ServletException;
+import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
+
+import static org.mockito.Mockito.mock;
+import static org.testng.Assert.assertEquals;
+
+public class EventCacheBroadcasterCacheTest {
+    private AtmosphereResource ar;
+    private Broadcaster broadcaster;
+    private AR atmosphereHandler;
+    private EventCacheBroadcasterCache eventCacheBroadcasterCache;
+
+    @BeforeMethod
+    public void setUp() throws Exception {
+        AtmosphereConfig config = new AtmosphereFramework().getAtmosphereConfig();
+        DefaultBroadcasterFactory factory = new DefaultBroadcasterFactory(DefaultBroadcaster.class, "NEVER", config);
+        broadcaster = factory.get(DefaultBroadcaster.class, "test");
+        eventCacheBroadcasterCache = new EventCacheBroadcasterCache();
+        broadcaster.getBroadcasterConfig().setBroadcasterCache(eventCacheBroadcasterCache);
+        atmosphereHandler = new AR();
+        ar = new AtmosphereResourceImpl(config,
+                broadcaster,
+                mock(AtmosphereRequest.class),
+                AtmosphereResponse.create(),
+                mock(BlockingIOCometSupport.class),
+                atmosphereHandler);
+        broadcaster.addAtmosphereResource(ar);
+    }
+
+    @AfterMethod
+    public void addAR(){
+        broadcaster.removeAtmosphereResource(ar);
+    }
+
+    @Test
+    public void testBasicCache() throws ExecutionException, InterruptedException, ServletException {
+        broadcaster.broadcast("e1").get();
+        broadcaster.removeAtmosphereResource(ar);
+        broadcaster.broadcast("e2").get();
+        broadcaster.broadcast("e3").get();
+
+        assertEquals(2, eventCacheBroadcasterCache.messages().get(ar.uuid()).getQueue().size());
+    }
+
+    @Test
+    public void addRemoveAddTest() throws ExecutionException, InterruptedException, ServletException {
+        broadcaster.broadcast("e1");
+        broadcaster.removeAtmosphereResource(ar);
+        broadcaster.broadcast("e2").get();
+        assertEquals(1, eventCacheBroadcasterCache.messages().size());
+
+        broadcaster.addAtmosphereResource(ar);
+        broadcaster.broadcast("e3").get();
+
+        assertEquals(0, eventCacheBroadcasterCache.messages().size());
+    }
+
+    @Test
+    public void nothingCachedTest() throws ExecutionException, InterruptedException, ServletException {
+        broadcaster.removeAtmosphereResource(ar);
+
+        broadcaster.broadcast("e1");
+        broadcaster.broadcast("e2");
+
+        assertEquals(0, eventCacheBroadcasterCache.messages().size());
+    }
+
+    @Test
+    public void concurrentCache() throws ExecutionException, InterruptedException, ServletException {
+        final CountDownLatch latch = new CountDownLatch(100);
+        broadcaster.addBroadcasterListener(new BroadcasterListener() {
+            @Override
+            public void onPostCreate(Broadcaster b) {
+            }
+
+            @Override
+            public void onComplete(Broadcaster b) {
+                latch.countDown();
+            }
+
+            @Override
+            public void onPreDestroy(Broadcaster b) {
+            }
+        }).broadcast("e-1");
+
+        broadcaster.removeAtmosphereResource(ar);
+
+        ExecutorService s = Executors.newCachedThreadPool();
+        for (int i=0; i < 99; i++) {
+            s.submit(new Runnable() {
+                @Override
+                public void run() {
+                    broadcaster.broadcast("e");
+                }
+            });
+        }
+
+        latch.await(10, TimeUnit.SECONDS);
+
+        assertEquals(100, eventCacheBroadcasterCache.messages().get(ar.uuid()).getQueue().size());
+    }
+
+    public final static class AR implements AtmosphereHandler {
+
+         public AtomicReference<StringBuffer> value = new AtomicReference<StringBuffer>(new StringBuffer());
+
+         @Override
+         public void onRequest(AtmosphereResource e) throws IOException {
+         }
+
+         @Override
+         public void onStateChange(AtmosphereResourceEvent e) throws IOException {
+             value.get().append(e.getMessage());
+         }
+
+         @Override
+         public void destroy() {
+         }
+     }
+
+
+
+}
