a32b5ed4bee4fc8e2956a928fd87ca95ab12bc82
jfarcand <jfarcand@apache.org>
2013-01-28 08:53:57 -0500

    Firts pass for #863

--- modules/cpr/src/main/java/org/atmosphere/cpr/AtmosphereFramework.java
+++ modules/cpr/src/main/java/org/atmosphere/cpr/AtmosphereFramework.java
@@ -592,9 +592,9 @@ public class AtmosphereFramework implements ServletContextProvider {
             interceptors.addFirst(newAInterceptor(SSEAtmosphereInterceptor.class));
             // Android 2.3.x streaming support
             interceptors.addFirst(newAInterceptor(AndroidAtmosphereInterceptor.class));
+            logger.info("Installed Default AtmosphereInterceptor {}. " +
+                    "Set org.atmosphere.cpr.AtmosphereInterceptor.disableDefaults in your xml to disable them.", interceptors);
         }
-        logger.info("Installed Default AtmosphereInterceptor {}. " +
-                "Set org.atmosphere.cpr.AtmosphereInterceptor.disableDefaults in your xml to disable them.", interceptors);
     }
 
     protected AtmosphereInterceptor newAInterceptor(Class<? extends AtmosphereInterceptor> a) {
