7aeba8b99d032d23582bdcc3f6986bddc12b356e
jfarcand <jfarcand@apache.org>
2013-02-07 16:22:08 -0500

    Fix possible NPE

--- modules/cpr/src/main/java/org/atmosphere/cpr/AsynchronousProcessor.java
+++ modules/cpr/src/main/java/org/atmosphere/cpr/AsynchronousProcessor.java
@@ -535,10 +535,9 @@ public abstract class AsynchronousProcessor implements AsyncSupport<AtmosphereRe
                     }
                 }
 
-                logger.debug("Cancelling the connection for AtmosphereResource {}", r.uuid());
-
                 r = (AtmosphereResourceImpl) req.resource();
                 if (r != null) {
+                    logger.debug("Cancelling the connection for AtmosphereResource {}", r.uuid());
 
                     if (r.isCancelled()) {
                         logger.trace("{} is already cancelled", r.uuid());
