bfced2e9f29349fb90f994ca4cdc26f4565035a5
jfarcand <jfarcand@apache.org>
2013-02-14 09:10:18 -0500

    Cosmetic, fix focus issue on IE

--- samples/chat/src/main/webapp/jquery/application.js
+++ samples/chat/src/main/webapp/jquery/application.js
@@ -48,13 +48,11 @@ $(function () {
             return;
         }
 
+        input.removeAttr('disabled').focus();
         if (!logged && myName) {
             logged = true;
             status.text(myName + ': ').css('color', 'blue');
-            input.removeAttr('disabled').focus();
         } else {
-            input.removeAttr('disabled');
-
             var me = json.author == author;
             var date = typeof(json.time) == 'string' ? parseInt(json.time) : json.time;
             addMessage(json.author, json.message, me ? 'blue' : 'black', new Date(date));
