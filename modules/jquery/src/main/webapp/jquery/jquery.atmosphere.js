14c3c764956c7d097c8dba40c540679f8cd38c0f
jfarcand <jfarcand@apache.org>
2013-02-21 13:00:56 -0500

    Fix for #927

--- modules/jquery/src/main/webapp/jquery/jquery.atmosphere.js
+++ modules/jquery/src/main/webapp/jquery/jquery.atmosphere.js
@@ -2212,7 +2212,9 @@ jQuery.atmosphere = function() {
                         break;
                     case "unsubscribe" :
                     case "closed" :
-                        if (typeof(f.onClose) != 'undefined') f.onClose(response);
+                        var closed = typeof(_request.closed) != 'undefined' ? _request.closed : false;
+                        if (typeof(f.onClose) != 'undefined' && !closed) f.onClose(response);
+                        _request.closed = true;
                         break;
                 }
             }
