4b2d1bfe11e31a828404f8e11cf2dab97a6cedd6
jfarcand <jfarcand@apache.org>
2013-03-18 18:20:52 -0400

    Fix for #966

--- modules/jquery/src/main/webapp/jquery/jquery.atmosphere.js
+++ modules/jquery/src/main/webapp/jquery/jquery.atmosphere.js
@@ -925,7 +925,14 @@ jQuery.atmosphere = function() {
                     }
                     return;
                 }
-                _sse = new EventSource(location, {withCredentials: _request.withCredentials});
+
+                try {
+                     _sse = new EventSource(location, {withCredentials: _request.withCredentials});
+                } catch (e) {
+                    _onError(e);
+                    _reconnectWithFallbackTransport("SSE failed. Downgrading to fallback transport and resending");
+                    return;
+                }
 
                 if (_request.connectTimeout > 0) {
                     _request.id = setTimeout(function() {
@@ -962,7 +969,7 @@ jQuery.atmosphere = function() {
                     _response.state = 'messageReceived';
                     _response.status = 200;
 
-                    var message = message.data;
+                    message = message.data;
                     var skipCallbackInvocation = _trackMessageSize(message, _request, _response);
 
                     if (jQuery.trim(message).length == 0) {
