a510dd0960a08fbca1f418871e98bc962ea4e39c
jfarcand <jfarcand@apache.org>
2013-05-16 11:44:03 -0400

    No need for multi chatroon yet

--- samples/chat/src/main/java/org/atmosphere/samples/chat/Chat.java
+++ samples/chat/src/main/java/org/atmosphere/samples/chat/Chat.java
@@ -29,7 +29,7 @@ import java.io.IOException;
  * Simple annotated class that demonstrate the power of Atmosphere. This class supports all transports, support
  * message length garantee, heart beat, message cache thanks to the @managedAService.
  */
-@ManagedService(path = "/{chat}")
+@ManagedService(path = "/chat")
 public class Chat {
     private final Logger logger = LoggerFactory.getLogger(Chat.class);
 
