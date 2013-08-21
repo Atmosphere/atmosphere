685a24950a9434705978ec4d083bac83a5802e6d
jfarcand <jfarcand@apache.org>
2013-08-21 10:44:11 -0400

    Fixes #1229

--- modules/cpr/src/main/java/org/atmosphere/util/annotation/AnnotationDetector.java
+++ modules/cpr/src/main/java/org/atmosphere/util/annotation/AnnotationDetector.java
@@ -54,15 +54,15 @@ import java.util.Map;
 import java.util.Set;
 
 /**
- * {@code AnnotationDetector} reads Java Class File (".class") files and reports the 
+ * {@code AnnotationDetector} reads Java Class File (".class") files and reports the
  * encountered annotations via a simple, developer friendly API.
  * <br/>
  * Main advantages of this library compared with similar solutions are:
  * <ul>
- * <li><u>light weight</u> (no dependencies, simple API, 15 kb jar file)</li> 
+ * <li><u>light weight</u> (no dependencies, simple API, 15 kb jar file)</li>
  * <li><u>very fast</u> (fastest annotation detection library as far as i know)</li>
  * </ul>
- *
+ * <p/>
  * A Java Class File consists of a stream of 8-bit bytes. All 16-bit, 32-bit, and 64-bit
  * quantities are constructed by reading in two, four, and eight consecutive 8-bit
  * bytes, respectively. Multi byte data items are always stored in big-endian order,
@@ -89,20 +89,20 @@ import java.util.Set;
  *   u2 attributes_count;
  *   attribute_info attributes[attributes_count];
  * }
- * 
+ *
  * Where:
  * u1 unsigned byte {@link java.io.DataInput#readUnsignedByte()}
  * u2 unsigned short {@link java.io.DataInput#readUnsignedShort()}
  * u4 unsigned int {@link java.io.DataInput#readInt()}
- * 
+ *
  * Annotations are stored as Attributes (i.e. "RuntimeVisibleAnnotations" and
  * "RuntimeInvisibleAnnotations").
  * </pre>
  * References:
  * <ul>
  * <li><a href="http://en.wikipedia.org/wiki/Java_class_file">Java class file (Wikipedia)</a> (Gentle Introduction);</li>
- * <li><a href="http://download.oracle.com/otndocs/jcp/jcfsu-1.0-fr-eval-oth-JSpec/">Class File 
- * Format Specification</a> (Java 6 version) and the 
+ * <li><a href="http://download.oracle.com/otndocs/jcp/jcfsu-1.0-fr-eval-oth-JSpec/">Class File
+ * Format Specification</a> (Java 6 version) and the
  * <a href="http://java.sun.com/docs/books/jvms/second_edition/html/ClassFile.doc.html">Java VM
  * Specification (Chapter 4)</a> for the real work.</li>
  * <li><a href="http://stackoverflow.com/questions/259140">scanning java annotations at runtime</a>.</li>
@@ -115,18 +115,18 @@ import java.util.Set;
  * <li><a herf="https://github.com/ngocdaothanh/annovention">annovention</a>, improved version of
  * the <a href="http://code.google.com/p/annovention">original Annovention</a> project. Available
  * from maven: {@code tv.cntt:annovention:1.2};</li>
- * <li>If using the Spring Framework, 
+ * <li>If using the Spring Framework,
  * <a href="http://static.springsource.org/spring/docs/2.5.x/api/org/springframework/context/annotation/ClassPathScanningCandidateComponentProvider.html">this</a>
  * is the way to go.</li>
  * </ul>
- * All above mentioned projects make use of a byte code manipulation library (like BCEL, 
+ * All above mentioned projects make use of a byte code manipulation library (like BCEL,
  * ASM or Javassist).
- * 
+ *
  * @author <a href="mailto:rmuller@xiam.nl">Ronald K. Muller</a>
  * @since annotation-detector 3.0.0
  */
 public final class AnnotationDetector {
-    
+
     /**
      * {@code Reporter} is the base interface, used to report the detected annotations.
      * Every category of annotations (i.e. Type, Field and Method) has its own specialized
@@ -139,9 +139,9 @@ public final class AnnotationDetector {
          * annotations are skipped).
          */
         Class<? extends Annotation>[] annotations();
-        
+
     }
-    
+
     public interface TypeReporter extends Reporter {
 
         /**
@@ -149,9 +149,9 @@ public final class AnnotationDetector {
          * Only {@code Annotation}s, specified by {@link #annotations()} are reported!
          */
         void reportTypeAnnotation(Class<? extends Annotation> annotation, String className);
-        
+
     }
-    
+
     public interface FieldReporter extends Reporter {
 
         /**
@@ -159,19 +159,19 @@ public final class AnnotationDetector {
          * Only {@code Annotation}s, specified by {@link #annotations()} are reported!
          */
         void reportFieldAnnotation(Class<? extends Annotation> annotation, String className, String fieldName);
-        
+
     }
-    
+
     public interface MethodReporter extends Reporter {
-        
+
         /**
          * This call back method is used to report an method level {@code Annotation}.
          * Only {@code Annotation}s, specified by {@link #annotations()} are reported!
          */
         void reportMethodAnnotation(Class<? extends Annotation> annotation, String className, String methodName);
-        
+
     }
-    
+
     // Only used during development. If set to "true" debug messages are displayed.
     private static final boolean DEBUG = false;
 
@@ -198,33 +198,33 @@ public final class AnnotationDetector {
     private static final int SHORT = 'S';
     private static final int BOOLEAN = 'Z';
     // used for AnnotationElement only
-    private static final int STRING = 's'; 
-    private static final int ENUM = 'e'; 
-    private static final int CLASS = 'c'; 
-    private static final int ANNOTATION = '@'; 
+    private static final int STRING = 's';
+    private static final int ENUM = 'e';
+    private static final int CLASS = 'c';
+    private static final int ANNOTATION = '@';
     private static final int ARRAY = '[';
 
     // The buffer is reused during the life cycle of this AnnotationDetector instance
     private final ClassFileBuffer cpBuffer = new ClassFileBuffer();
     // the annotation types to report, see {@link #annotations()}
     private final Map<String, Class<? extends Annotation>> annotations;
-    
+
     private TypeReporter typeReporter;
     private FieldReporter fieldReporter;
     private MethodReporter methodReporter;
 
     // the 'raw' name of this interface or class (using '/' instead of '.' in package name)
-    private String typeName; 
+    private String typeName;
     // Reusing the constantPool is not needed for better performance
     private Object[] constantPool;
     private String memberName;
-    
+
     /**
-     * Create a new {@code AnnotationDetector}, reporting the detected annotations 
+     * Create a new {@code AnnotationDetector}, reporting the detected annotations
      * to the specified {@code Reporter}.
      */
     public AnnotationDetector(final Reporter reporter) {
-        
+
         final Class<? extends Annotation>[] a = reporter.annotations();
         annotations = new HashMap<String, Class<? extends Annotation>>(a.length);
         // map "raw" type names to Class object
@@ -232,22 +232,22 @@ public final class AnnotationDetector {
             annotations.put("L" + a[i].getName().replace('.', '/') + ";", a[i]);
         }
         if (reporter instanceof TypeReporter) {
-            typeReporter = (TypeReporter)reporter;
+            typeReporter = (TypeReporter) reporter;
         }
         if (reporter instanceof FieldReporter) {
-            fieldReporter = (FieldReporter)reporter;
+            fieldReporter = (FieldReporter) reporter;
         }
         if (reporter instanceof MethodReporter) {
-            methodReporter = (MethodReporter)reporter;
+            methodReporter = (MethodReporter) reporter;
         }
         if (typeReporter == null && fieldReporter == null && methodReporter == null) {
             throw new AssertionError("No reporter defined");
         }
     }
-    
+
     /**
      * Report all Java ClassFile files available on the class path.
-     * 
+     *
      * @see #detect(java.io.File...)
      */
     public final void detect() throws IOException {
@@ -268,7 +268,13 @@ public final class AnnotationDetector {
             if (!internalPackageName.endsWith("/")) {
                 internalPackageName = internalPackageName.concat("/");
             }
-            final Enumeration<URL> resourceEnum = classLoader.getResources(internalPackageName);
+
+            Enumeration<URL> resourceEnum = classLoader.getResources(internalPackageName);
+            /* weblogic workaround */
+            if (!resourceEnum.hasMoreElements()) {
+                resourceEnum = this.getClass().getClassLoader().getResources(internalPackageName);
+            }
+
             while (resourceEnum.hasMoreElements()) {
                 final URL url = resourceEnum.nextElement();
                 // Handle JBoss VFS URL's which look like (example package 'nl.dvelop'):
@@ -296,7 +302,7 @@ public final class AnnotationDetector {
                     }
                 } else {
                     // Resource in Jar File
-                    final File jarFile = toFile(((JarURLConnection)url.openConnection()).getJarFileURL());
+                    final File jarFile = toFile(((JarURLConnection) url.openConnection()).getJarFileURL());
                     if (jarFile.isFile()) {
                         files.add(jarFile);
                         if (DEBUG) print("Add jar file: '%s'", jarFile);
@@ -319,7 +325,7 @@ public final class AnnotationDetector {
      * {@code CAFEBABE} are silently ignored.
      */
     public final void detect(final File... filesOrDirectories) throws IOException {
-        if (DEBUG) print("detectFilesOrDirectories: %s", (Object)filesOrDirectories);
+        if (DEBUG) print("detectFilesOrDirectories: %s", (Object) filesOrDirectories);
         detect(new ClassFileIterator(filesOrDirectories));
     }
 
@@ -355,7 +361,7 @@ public final class AnnotationDetector {
     }
 
     private boolean hasCafebabe(final ClassFileBuffer buffer) throws IOException {
-        return buffer.size() > 4 &&  buffer.readInt() == 0xCAFEBABE;
+        return buffer.size() > 4 && buffer.readInt() == 0xCAFEBABE;
     }
 
     /**
@@ -376,7 +382,7 @@ public final class AnnotationDetector {
     private void readVersion(final DataInput di) throws IOException {
         // sequence: minor version, major version (argument_index is 1-based)
         if (DEBUG) {
-            print("Java Class version %2$d.%1$d", di.readUnsignedShort(), di.readUnsignedShort()); 
+            print("Java Class version %2$d.%1$d", di.readUnsignedShort(), di.readUnsignedShort());
         } else {
             di.skipBytes(4);
         }
@@ -427,14 +433,14 @@ public final class AnnotationDetector {
                 return false;
             default:
                 throw new ClassFormatError(
-                    "Unkown tag value for constant pool entry: " + tag);
+                        "Unkown tag value for constant pool entry: " + tag);
         }
     }
 
     private void readAccessFlags(final DataInput di) throws IOException {
         di.skipBytes(2); // u2
     }
-    
+
     private void readThisClass(final DataInput di) throws IOException {
         typeName = resolveUtf8(di);
         if (DEBUG) print("read type '%s'", typeName);
@@ -459,7 +465,7 @@ public final class AnnotationDetector {
             readAttributes(di, 'F', fieldReporter == null);
             if (DEBUG) print("Field: %s, descriptor: %s", memberName, descriptor);
         }
-    }   
+    }
 
     private void readMethods(final DataInput di) throws IOException {
         final int count = di.readUnsignedShort();
@@ -471,20 +477,20 @@ public final class AnnotationDetector {
             readAttributes(di, 'M', methodReporter == null);
             if (DEBUG) print("Method: %s, descriptor: %s", memberName, descriptor);
         }
-    }   
-    
-    private void readAttributes(final DataInput di, final char reporterType, final boolean skipReporting) 
-        throws IOException {
-        
+    }
+
+    private void readAttributes(final DataInput di, final char reporterType, final boolean skipReporting)
+            throws IOException {
+
         final int count = di.readUnsignedShort();
         if (DEBUG) print("attribute count (%s) = %d", reporterType, count);
         for (int i = 0; i < count; ++i) {
             final String name = resolveUtf8(di);
             // in bytes, use this to skip the attribute info block
             final int length = di.readInt();
-            if (!skipReporting && 
-                ("RuntimeVisibleAnnotations".equals(name) ||
-                "RuntimeInvisibleAnnotations".equals(name))) {
+            if (!skipReporting &&
+                    ("RuntimeVisibleAnnotations".equals(name) ||
+                            "RuntimeInvisibleAnnotations".equals(name))) {
                 readAnnotations(di, reporterType);
             } else {
                 if (DEBUG) print("skip attribute %s", name);
@@ -519,7 +525,7 @@ public final class AnnotationDetector {
             }
         }
     }
-    
+
     private String readAnnotation(final DataInput di) throws IOException {
         final String rawTypeName = resolveUtf8(di);
         // num_element_value_pairs
@@ -538,7 +544,7 @@ public final class AnnotationDetector {
 
     private void readAnnotationElementValue(final DataInput di) throws IOException {
         final int tag = di.readUnsignedByte();
-        if (DEBUG) print("tag='%c'", (char)tag);
+        if (DEBUG) print("tag='%c'", (char) tag);
         switch (tag) {
             case BYTE:
             case CHAR:
@@ -580,17 +586,19 @@ public final class AnnotationDetector {
         final Object value = constantPool[index];
         final String s;
         if (value instanceof Integer) {
-            s = (String)constantPool[(Integer)value];
+            s = (String) constantPool[(Integer) value];
             if (DEBUG) print("resolveUtf8(%d): %d --> %s", index, value, s);
         } else {
-            s = (String)value;
+            s = (String) value;
             if (DEBUG) print("resolveUtf8(%d): %s", index, s);
         }
-        
+
         return s;
     }
-    
-    /** Helper method for simple (debug) logging. */
+
+    /**
+     * Helper method for simple (debug) logging.
+     */
     private static void print(final String message, final Object... args) {
         if (DEBUG) {
             final String logMessage;
@@ -604,9 +612,9 @@ public final class AnnotationDetector {
                     }
                     if (args[i].getClass().isArray()) {
                         // cast back to array! Note that primitive arrays are not supported
-                        args[i] = Arrays.toString((Object[])args[i]);
+                        args[i] = Arrays.toString((Object[]) args[i]);
                     } else if (args[i] == Class.class) {
-                        args[i] = ((Class<?>)args[i]).getName();
+                        args[i] = ((Class<?>) args[i]).getName();
                     }
                 }
                 logMessage = String.format(message, args);
@@ -618,7 +626,7 @@ public final class AnnotationDetector {
     /**
      * Reclaim memory.
      */
-    public void destroy(){
+    public void destroy() {
         annotations.clear();
         constantPool = null;
         cpBuffer.destroy();
