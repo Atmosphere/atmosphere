367c64b6f0d7b62235fe871a7c7baeef577af88f
jfarcand <jfarcand@apache.org>
2013-08-22 08:55:03 -0400

    Align with 1.0.x

--- modules/cpr/src/main/java/org/atmosphere/cpr/AtmosphereFramework.java
+++ modules/cpr/src/main/java/org/atmosphere/cpr/AtmosphereFramework.java
@@ -142,7 +142,7 @@ public class AtmosphereFramework implements ServletContextProvider {
     public static final String DEFAULT_ATMOSPHERE_CONFIG_PATH = "/META-INF/atmosphere.xml";
     public static final String DEFAULT_LIB_PATH = "/WEB-INF/lib/";
     public static final String DEFAULT_HANDLER_PATH = "/WEB-INF/classes/";
-    public static final String MAPPING_REGEX = "[a-zA-Z0-9-&.*_=@;\\?]+";
+    public static final String MAPPING_REGEX = "[a-zA-Z0-9-&.*_~=@;\\?]+";
     public static final String ROOT = "/*";
 
     protected static final Logger logger = LoggerFactory.getLogger(AtmosphereFramework.class);
