cb32c219b5cbbd6be9be6e911f531ea112caaaf5
jfarcand <jfarcand@apache.org>
2013-09-04 09:45:33 -0400

    Fix for #1245

--- modules/jersey/src/main/java/org/atmosphere/jersey/AtmosphereFilter.java
+++ modules/jersey/src/main/java/org/atmosphere/jersey/AtmosphereFilter.java
@@ -939,6 +939,11 @@ public class AtmosphereFilter implements ResourceFilterFactory {
                         r.removeEventListener(a);
                         response.setResponse(b.entity(entity).build());
                         response.write();
+                        try {
+                            response.getOutputStream().flush();
+                        } catch (IOException ex) {
+                            logger.trace("", ex);
+                        }
                     }
                 }
 
