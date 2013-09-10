9ad66ccebc3547d555ec747fa3b3939ca1a9f876
jfarcand <jfarcand@apache.org>
2013-09-10 14:34:41 -0400

    Fixes Atmosphere/nettosphere#40

--- modules/cpr/src/main/java/org/atmosphere/cpr/AtmosphereFramework.java
+++ modules/cpr/src/main/java/org/atmosphere/cpr/AtmosphereFramework.java
@@ -201,19 +201,20 @@ public class AtmosphereFramework implements ServletContextProvider {
     protected boolean executeFirstSet = false;
 
     protected final Class<? extends AtmosphereInterceptor>[] defaultInterceptors = new Class[]{
-            OnDisconnectInterceptor.class,
-            // ADD Tracking ID Handshake
-            JavaScriptProtocol.class,
-            // ADD JSONP support
-            JSONPAtmosphereInterceptor.class,
-            // Add SSE support
-            SSEAtmosphereInterceptor.class,
-            // Android 2.3.x streaming support
-            AndroidAtmosphereInterceptor.class,
+            // Default Interceptor
+            DefaultHeadersInterceptor.class,
             // WebKit & IE Padding
             PaddingAtmosphereInterceptor.class,
-            // Default Interceptor
-            DefaultHeadersInterceptor.class
+            // Android 2.3.x streaming support
+            AndroidAtmosphereInterceptor.class,
+            // Add SSE support
+            SSEAtmosphereInterceptor.class,
+            // ADD JSONP support
+            JSONPAtmosphereInterceptor.class,
+            // ADD Tracking ID Handshake
+            JavaScriptProtocol.class,
+            // OnDisconnect
+            OnDisconnectInterceptor.class
     };
 
     /**
@@ -786,7 +787,7 @@ public class AtmosphereFramework implements ServletContextProvider {
         s = sc.getInitParameter(ApplicationConfig.DISABLE_ATMOSPHEREINTERCEPTOR);
         if (s == null) {
             for (Class<? extends AtmosphereInterceptor> a : defaultInterceptors) {
-                interceptors.addFirst(newAInterceptor(a));
+                interceptors.addLast(newAInterceptor(a));
             }
             logger.info("Set {} to disable them.", ApplicationConfig.DISABLE_ATMOSPHEREINTERCEPTOR, interceptors);
         }
