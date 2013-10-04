e79aecbbccb3fb504278eb67927959aefca2f9e7
jfarcand <jfarcand@apache.org>
2013-10-04 09:01:24 -0400

    Fixes #1329

--- modules/cpr/src/main/java/org/atmosphere/cpr/AtmosphereFramework.java
+++ modules/cpr/src/main/java/org/atmosphere/cpr/AtmosphereFramework.java
@@ -2003,6 +2003,10 @@ public class AtmosphereFramework {
             }
         }
 
+        if (isInit) {
+            c.configure(config);
+        }
+
         if (!found) {
             InvokationOrder.PRIORITY p = InvokationOrder.class.isAssignableFrom(c.getClass()) ? InvokationOrder.class.cast(c).priority() : InvokationOrder.AFTER_DEFAULT;
             positionInterceptor(p, c);
