7cbfbc7affda7d124bced9a658bfcd71e69dee44
jfarcand <jfarcand@apache.org>
2013-10-11 12:56:29 -0400

    Improve fix for #1342

--- modules/cpr/src/main/java/org/atmosphere/interceptor/AtmosphereResourceStateRecovery.java
+++ modules/cpr/src/main/java/org/atmosphere/interceptor/AtmosphereResourceStateRecovery.java
@@ -193,16 +193,15 @@ public class AtmosphereResourceStateRecovery implements AtmosphereInterceptor {
         public void onRemoveAtmosphereResource(Broadcaster b, AtmosphereResource r) {
             // We track cancelled and resumed connection only.
             BroadcasterTracker t = states.get(r.uuid());
-
-            // The BroadcasterTracker was swapped
-            if (t == null) {
-                onAddAtmosphereResource(b, r);
-            }
-
             AtmosphereResourceEvent e = r.getAtmosphereResourceEvent();
             if (t != null && (e.isClosedByClient() || !r.isResumed() && !e.isResumedOnTimeout())) {
                 t.remove(b);
             } else {
+
+                // The BroadcasterTracker was swapped
+                if (t == null) {
+                    onAddAtmosphereResource(b, r);
+                }
                 logger.trace("Keeping the state of {} with broadcaster {}", r.uuid(), b.getID());
                 logger.trace("State for {} with broadcaster {}", r.uuid(), t != null ? t.ids() : "null");
             }
