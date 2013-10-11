a19a464e052362bfd2bcad20455e3a1e04af99be
jfarcand <jfarcand@apache.org>
2013-10-11 13:08:44 -0400

    Prevent NPE

--- modules/cpr/src/main/java/org/atmosphere/interceptor/AtmosphereResourceStateRecovery.java
+++ modules/cpr/src/main/java/org/atmosphere/interceptor/AtmosphereResourceStateRecovery.java
@@ -194,7 +194,6 @@ public class AtmosphereResourceStateRecovery implements AtmosphereInterceptor {
                 logger.trace("Deleting the state of {} with broadcaster {}", r.uuid(), b.getID());
                 t.remove(b);
             } else {
-
                 // The BroadcasterTracker was swapped
                 if (t == null) {
                     onAddAtmosphereResource(b, r);
@@ -259,7 +258,7 @@ public class AtmosphereResourceStateRecovery implements AtmosphereInterceptor {
             Broadcaster b = factory.lookup(broadcasterID, false);
             BroadcasterCache cache;
             logger.trace("About to retrieve cached messages for resource {} with Broadcaster {}, tracked by " + b, r.uuid(), r.getBroadcaster());
-            if (force || (b != null && !b.getID().equalsIgnoreCase(r.getBroadcaster().getID()))) {
+            if (b != null && (force || !b.getID().equalsIgnoreCase(r.getBroadcaster().getID()))) {
                 // We cannot add the resource now. we need to first make sure there is no cached message.
                 cache = b.getBroadcasterConfig().getBroadcasterCache();
                 List<Object> t = cache.retrieveFromCache(b.getID(), r);
