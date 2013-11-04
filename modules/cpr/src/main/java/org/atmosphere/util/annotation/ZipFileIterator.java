18873b7d4d2f9820afeba1870c668a4ba3c8e35a
jfarcand <jfarcand@apache.org>
2013-11-04 08:33:20 -0500

    Port Fixes for rmuller/infomas-asl#14

--- modules/cpr/src/main/java/org/atmosphere/util/annotation/ZipFileIterator.java
+++ modules/cpr/src/main/java/org/atmosphere/util/annotation/ZipFileIterator.java
@@ -14,21 +14,19 @@
  * the License.
  */
 /* ZipFileIterator.java
- * 
- ******************************************************************************
  *
- * Created: Oct 10, 2011
+ * Created: 2011-10-10 (Year-Month-Day)
  * Character encoding: UTF-8
- * 
- * Copyright (c) 2011 - XIAM Solutions B.V. The Netherlands, http://www.xiam.nl
- * 
- ********************************* LICENSE ************************************
+ *
+ ****************************************** LICENSE *******************************************
+ *
+ * Copyright (c) 2011 - 2013 XIAM Solutions B.V. (http://www.xiam.nl)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -38,7 +36,6 @@
  */
 package org.atmosphere.util.annotation;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Enumeration;
@@ -46,8 +43,11 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
 /**
- * {@code ZipFileIterator} is used to iterate over all entries in a {@code zip}
- * or {@code jar} file and returning the {@link java.io.InputStream} of these entries.
+ * {@code ZipFileIterator} is used to iterate over all entries in a given {@code zip} or
+ * {@code jar} file and returning the {@link InputStream} of these entries.
+ * <p>
+ * It is possible to specify an (optional) entry name filter.
+ * <p>
  * The most efficient way of iterating is used, see benchmark in test classes.
  *
  * @author <a href="mailto:rmuller@xiam.nl">Ronald K. Muller</a>
@@ -56,12 +56,23 @@ import java.util.zip.ZipFile;
 final class ZipFileIterator {
 
     private final ZipFile zipFile;
+    private final String[] entryNameFilter;
     private final Enumeration<? extends ZipEntry> entries;
+
     private ZipEntry current;
 
-    ZipFileIterator(final File file) throws IOException {
-        zipFile = new ZipFile(file);
-        entries = zipFile.entries();
+    /**
+     * Create a new {@code ZipFileIterator} instance.
+     *
+     * @param zipFile The ZIP file used to iterate over all entries
+     * @param entryNameFilter (optional) file name filter. Only entry names starting with
+     * one of the specified names in the filter are returned
+     */
+    ZipFileIterator(final ZipFile zipFile, final String[] entryNameFilter) throws IOException {
+        this.zipFile = zipFile;
+        this.entryNameFilter = entryNameFilter;
+
+        this.entries = zipFile.entries();
     }
 
     public ZipEntry getEntry() {
@@ -71,7 +82,7 @@ final class ZipFileIterator {
     public InputStream next() throws IOException {
         while (entries.hasMoreElements()) {
             current = entries.nextElement();
-            if (!current.isDirectory()) {
+            if (accept(current)) {
                 return zipFile.getInputStream(current);
             }
         }
@@ -79,10 +90,25 @@ final class ZipFileIterator {
         try {
             // zipFile is never null here
             zipFile.close();
-        } catch (IOException ex) {
+        } catch (IOException ex) { // SUPPRESS CHECKSTYLE EmptyBlockCheck
             // suppress IOException, otherwise close() is called twice
         }
         return null;
+    }
+
+    private boolean accept(final ZipEntry entry) {
+        if (entry.isDirectory()) {
+            return false;
+        }
+        if (entryNameFilter == null) {
+            return true;
+        }
+        for (final String filter : entryNameFilter) {
+            if (entry.getName().startsWith(filter)) {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
