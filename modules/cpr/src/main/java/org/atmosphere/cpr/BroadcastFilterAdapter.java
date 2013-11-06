d20edc8e6ccc8235cd672bee13ffde8d7ba58b46
jfarcand <jfarcand@apache.org>
2013-11-06 14:01:10 -0500

    Fix wrong package name

--- modules/cpr/src/main/java/org/atmosphere/cpr/BroadcastFilterAdapter.java
+++ modules/cpr/src/main/java/org/atmosphere/cpr/BroadcastFilterAdapter.java
@@ -13,10 +13,7 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-package org.atmosphere;
-
-import org.atmosphere.cpr.AtmosphereResource;
-import org.atmosphere.cpr.PerRequestBroadcastFilter;
+package org.atmosphere.cpr;
 
 /**
  * Adapter for {@link PerRequestBroadcastFilter}. Both methods return {@link BroadcastAction.ACTION#CONTINUE} with the message.
