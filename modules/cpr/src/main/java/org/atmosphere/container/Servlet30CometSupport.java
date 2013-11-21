855f48e9a488c022f14f534466ae0209ea506bf4
jfarcand <jfarcand@apache.org>
2013-11-21 14:16:51 -0500

    Better fix

--- modules/cpr/src/main/java/org/atmosphere/container/Servlet30CometSupport.java
+++ modules/cpr/src/main/java/org/atmosphere/container/Servlet30CometSupport.java
@@ -197,9 +197,10 @@ public class Servlet30CometSupport extends AsynchronousProcessor {
             try {
                 p.timedout((AtmosphereRequest) event.getAsyncContext().getRequest(),
                         (AtmosphereResponse) event.getAsyncContext().getResponse());
-                event.getAsyncContext().complete();
             } catch (ServletException ex) {
                 logger.warn("onTimeout(): failed timing out comet response: " + event.getAsyncContext().getResponse(), ex);
+            }  finally {
+                event.getAsyncContext().complete();
             }
         }
 
