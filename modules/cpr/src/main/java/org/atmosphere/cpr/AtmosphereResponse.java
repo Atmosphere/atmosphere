61f024f269f9444fdc2dc2a23eb3c4321b02ff6c
jfarcand <jfarcand@apache.org>
2013-12-03 10:35:14 -0500

    Improve logging

--- modules/cpr/src/main/java/org/atmosphere/cpr/AtmosphereResponse.java
+++ modules/cpr/src/main/java/org/atmosphere/cpr/AtmosphereResponse.java
@@ -870,7 +870,7 @@ public class AtmosphereResponse extends HttpServletResponseWrapper {
             AtmosphereResourceImpl.class.cast(r).notifyListeners(
                     new AtmosphereResourceEventImpl(AtmosphereResourceImpl.class.cast(r), true, false));
         }
-        logger.debug("", ex);
+        logger.trace("", ex);
     }
 
     /**
