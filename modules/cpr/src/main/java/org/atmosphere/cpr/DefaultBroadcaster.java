0aaa94462bb08336d43b9c8becbbd538108fa318
jfarcand <jfarcand@apache.org>
2014-01-13 14:34:49 -0500

    Fixes #1429

--- modules/cpr/src/main/java/org/atmosphere/cpr/DefaultBroadcaster.java
+++ modules/cpr/src/main/java/org/atmosphere/cpr/DefaultBroadcaster.java
@@ -995,7 +995,7 @@ public class DefaultBroadcaster implements Broadcaster {
             // Must make sure execute only one thread
             synchronized (r) {
                 try {
-                    r.getRequest().setAttribute(CACHED, "true");
+                    rImpl.getRequest().setAttribute(CACHED, "true");
                     prepareInvokeOnStateChange(r, e);
                 } catch (Throwable t) {
                     // An exception occurred
@@ -1408,7 +1408,7 @@ public class DefaultBroadcaster implements Broadcaster {
         if (!wasResumed && isAtmosphereResourceValid(r)) {
             logger.trace("Associating AtmosphereResource {} with Broadcaster {}", r.uuid(), getID());
 
-            String parentUUID = (String) r.getRequest().getAttribute(SUSPENDED_ATMOSPHERE_RESOURCE_UUID);
+            String parentUUID = (String) AtmosphereResourceImpl.class.cast(r).getRequest(false).getAttribute(SUSPENDED_ATMOSPHERE_RESOURCE_UUID);
             if (!backwardCompatible && parentUUID != null) {
                 AtmosphereResource p = AtmosphereResourceFactory.getDefault().find(parentUUID);
                 if (p != null && !resources.contains(p)) {
