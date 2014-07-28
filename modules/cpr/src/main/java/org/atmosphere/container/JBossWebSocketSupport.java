e65f081d8a8bd790cbfcf5f721d78630a2cd4477
mvsmasiero <mvsmasiero@gmail.com>
2014-07-28 09:57:53 -0300

    Revert "Fixes #1670"
    This reverts commit a5feff807d13dd18423583416be19342979c0dbf.

--- modules/cpr/src/main/java/org/atmosphere/container/JBossWebSocketSupport.java
+++ modules/cpr/src/main/java/org/atmosphere/container/JBossWebSocketSupport.java
@@ -37,7 +37,7 @@ import java.lang.reflect.Constructor;
  * @author Péter Miklós
  * @see https://github.com/mikebrock/jboss-websockets
  */
-public class JBossWebSocketSupport extends JBossWebCometSupport {
+public class JBossWebSocketSupport extends AsynchronousProcessor {
 
 
     private static final Logger logger = LoggerFactory.getLogger(JBossWebSocketSupport.class);
@@ -63,6 +63,11 @@ public class JBossWebSocketSupport extends JBossWebCometSupport {
         }
         
         return null;
+    }
+
+    @Override
+    public Action service(AtmosphereRequest req, AtmosphereResponse res) throws IOException, ServletException {
+        return suspended(req, res);
     }
 
     @Override
