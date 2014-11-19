f89cefb5293f54d0639a087a41168c4cccd83cec
jfarcand <jfarcand.oss@gmail.com>
2014-11-19 17:08:55 -0500

    Fixes #1777

--- modules/cpr/src/main/java/org/atmosphere/util/annotation/AnnotationDetector.java
+++ modules/cpr/src/main/java/org/atmosphere/util/annotation/AnnotationDetector.java
@@ -187,7 +187,7 @@ public final class AnnotationDetector {
 
     // Only used during development. If set to "true" debug messages are displayed.
     private static final boolean DEBUG = false;
-    private final Logger logger = LoggerFactory.getLogger(AnnotationDetector.class);
+    private final static Logger logger = LoggerFactory.getLogger(AnnotationDetector.class);
 
     // Constant Pool type tags
     private static final int CP_UTF8 = 1;
@@ -785,7 +785,7 @@ public final class AnnotationDetector {
                 }
                 logMessage = String.format(message, args);
             }
-            System.out.println(logMessage); // SUPPRESS CHECKSTYLE RegexpSinglelineJavaCheck
+            logger.debug(logMessage); // SUPPRESS CHECKSTYLE RegexpSinglelineJavaCheck
         }
     }
 
