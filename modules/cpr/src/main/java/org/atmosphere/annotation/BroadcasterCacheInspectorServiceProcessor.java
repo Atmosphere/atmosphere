5b543c9a123e979e742be47532ffe3056e8377cb
jfarcand <jfarcand@apache.org>
2015-01-09 15:40:22 -0500

    Fix cut & paste stupid mistake

--- modules/cpr/src/main/java/org/atmosphere/annotation/BroadcasterCacheInspectorServiceProcessor.java
+++ modules/cpr/src/main/java/org/atmosphere/annotation/BroadcasterCacheInspectorServiceProcessor.java
@@ -19,19 +19,18 @@ import org.atmosphere.cache.BroadcasterCacheInspector;
 import org.atmosphere.config.AtmosphereAnnotation;
 import org.atmosphere.config.service.BroadcasterCacheInspectorService;
 import org.atmosphere.cpr.AtmosphereFramework;
-import org.atmosphere.cpr.BroadcasterCache;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @AtmosphereAnnotation(BroadcasterCacheInspectorService.class)
-public class BroadcasterCacheInspectorServiceProcessor implements Processor<BroadcasterCache> {
+public class BroadcasterCacheInspectorServiceProcessor implements Processor<BroadcasterCacheInspector> {
 
     private static final Logger logger = LoggerFactory.getLogger(BroadcasterCacheInspectorServiceProcessor.class);
 
     @Override
-    public void handle(AtmosphereFramework framework, Class<BroadcasterCache> annotatedClass) {
+    public void handle(AtmosphereFramework framework, Class<BroadcasterCacheInspector> annotatedClass) {
         try {
-            framework.addBroadcasterCacheInjector((BroadcasterCacheInspector) framework.newClassInstance(BroadcasterCache.class, annotatedClass));
+            framework.addBroadcasterCacheInjector(framework.newClassInstance(BroadcasterCacheInspector.class, annotatedClass));
         } catch (Throwable e) {
             logger.warn("", e);
         }
