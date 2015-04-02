3854f5e10ead8f7912d5df167680cc95ad704606
jfarcand <jfarcand@apache.org>
2015-04-02 07:00:47 +0200

    Fixes #1933

--- modules/cpr/src/main/java/org/atmosphere/container/Grizzly2WebSocketSupport.java
+++ modules/cpr/src/main/java/org/atmosphere/container/Grizzly2WebSocketSupport.java
@@ -21,6 +21,7 @@ import org.atmosphere.cpr.AtmosphereConfig;
 import org.atmosphere.cpr.AtmosphereRequest;
 import org.atmosphere.cpr.AtmosphereResponse;
 import org.atmosphere.cpr.WebSocketProcessorFactory;
+import org.atmosphere.util.IOUtils;
 import org.atmosphere.util.Utils;
 import org.atmosphere.websocket.WebSocketProcessor;
 import org.glassfish.grizzly.http.HttpRequestPacket;
@@ -47,7 +48,7 @@ public class Grizzly2WebSocketSupport extends Grizzly2CometSupport {
     public Grizzly2WebSocketSupport(AtmosphereConfig config) {
         super(config);
         application = new Grizzly2WebSocketApplication(config);
-        WebSocketEngine.getEngine().register(application);
+        WebSocketEngine.getEngine().register(config.getServletContext().getContextPath(), IOUtils.guestServletPath(config), application);
     }
 
 
@@ -88,7 +89,6 @@ public class Grizzly2WebSocketSupport extends Grizzly2CometSupport {
     private static final class Grizzly2WebSocketApplication extends WebSocketApplication {
 
         private AtmosphereConfig config;
-        private final String contextPath;
         private final WebSocketProcessor webSocketProcessor;
 
         // -------------------------------------------------------- Constructors
@@ -96,7 +96,6 @@ public class Grizzly2WebSocketSupport extends Grizzly2CometSupport {
 
         public Grizzly2WebSocketApplication(AtmosphereConfig config) {
             this.config = config;
-            contextPath = config.getServletContext().getContextPath();
             this.webSocketProcessor = WebSocketProcessorFactory.getDefault()
                     .getWebSocketProcessor(config.framework());
         }
@@ -109,11 +108,6 @@ public class Grizzly2WebSocketSupport extends Grizzly2CometSupport {
             if (!webSocketProcessor.handshake(null)) {
                 throw new org.glassfish.grizzly.websockets.HandshakeException("WebSocket not accepted");
             }
-        }
-
-        @Override
-        public boolean isApplicationRequest(HttpRequestPacket request) {
-            return request.getRequestURI().startsWith(contextPath);
         }
 
         @Override
