0d97287a384b20dd9631a80f5a2ff663cffdab4a
jfarcand <jfarcand@apache.org>
2015-08-05 15:14:55 -0400

    Fix possible NPE

--- modules/cpr/src/main/java/org/atmosphere/container/version/JSR356WebSocket.java
+++ modules/cpr/src/main/java/org/atmosphere/container/version/JSR356WebSocket.java
@@ -159,10 +159,12 @@ public class JSR356WebSocket extends WebSocket {
         @Override
         public void onResult(SendResult result) {
             semaphore.release();
-            if (!result.isOK() || result.getException() != null && r != null) {
+            if (!result.isOK() || result.getException() != null) {
                 logger.trace("WebSocket {} failed to write {}", r, message);
-                Broadcaster b = r.getBroadcaster();
-                b.getBroadcasterConfig().getBroadcasterCache().addToCache(b.getID(), r.uuid(), new BroadcastMessage(message));
+                if (r != null) {
+                    Broadcaster b = r.getBroadcaster();
+                    b.getBroadcasterConfig().getBroadcasterCache().addToCache(b.getID(), r.uuid(), new BroadcastMessage(message));
+                }
             }
         }
     }
