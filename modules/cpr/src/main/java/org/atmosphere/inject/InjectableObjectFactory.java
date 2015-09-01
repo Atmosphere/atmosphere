fcb93164fbaed81153e11684cc5ffdc8ae745419
jfarcand <jfarcand@apache.org>
2015-09-01 16:42:33 -0400

    Fixes #2041

--- modules/cpr/src/main/java/org/atmosphere/inject/InjectableObjectFactory.java
+++ modules/cpr/src/main/java/org/atmosphere/inject/InjectableObjectFactory.java
@@ -52,6 +52,7 @@ public class InjectableObjectFactory implements AtmosphereObjectFactory<Injectab
     private final LinkedList<Injectable<?>> injectables = new LinkedList<Injectable<?>>();
     private final LinkedList<InjectIntrospector<?>> introspectors = new LinkedList<InjectIntrospector<?>>();
     private final LinkedList<InjectIntrospector<?>> requestScopedIntrospectors = new LinkedList<InjectIntrospector<?>>();
+    private final LinkedList<Object> pushBackInjection = new LinkedList<>();
 
     private AtmosphereConfig config;
 
@@ -89,6 +90,27 @@ public class InjectableObjectFactory implements AtmosphereObjectFactory<Injectab
                 logger.error("", e);
             }
         }
+
+        config.startupHook(new AtmosphereConfig.StartupHook() {
+            @Override
+            public void started(AtmosphereFramework framework) {
+                Set<Field> fields = new HashSet<Field>();
+                try {
+                    for (Object instance : pushBackInjection) {
+                        fields.addAll(getInheritedPrivateFields(instance.getClass()));
+                        try {
+                            injectFields(fields, instance, framework, injectables);
+                        } catch (IllegalAccessException e) {
+                            logger.warn("", e);
+                        }
+                        fields.clear();
+                    }
+                } finally {
+                    pushBackInjection.clear();
+                }
+
+            }
+        });
     }
 
     @Override
@@ -170,7 +192,10 @@ public class InjectableObjectFactory implements AtmosphereObjectFactory<Injectab
                             field.setAccessible(true);
                             Object o = c.injectable(framework.getAtmosphereConfig());
 
-                            if (o == null) continue;
+                            if (o == null) {
+                                pushBackInjection.addFirst(instance);
+                                continue;
+                            }
 
                             if (field.getType().equals(Boolean.TYPE)) {
                                 field.setBoolean(instance, Boolean.class.cast(o).booleanValue());
