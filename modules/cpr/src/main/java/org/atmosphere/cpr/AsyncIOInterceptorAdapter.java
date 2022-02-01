0190c054824f3b3cf6eb56f8dd3a8144c05b9d9e
jfarcand <jfarcand@apache.org>
2022-02-01 16:25:37 -0500

    Update Copyright

--- modules/cpr/src/main/java/org/atmosphere/cpr/AsyncIOInterceptorAdapter.java
+++ modules/cpr/src/main/java/org/atmosphere/cpr/AsyncIOInterceptorAdapter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2008-2021 Async-IO.org
+ * Copyright 2008-2022 Async-IO.org
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
