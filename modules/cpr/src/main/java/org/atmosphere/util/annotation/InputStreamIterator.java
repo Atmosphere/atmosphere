Atmosphere/atmosphere

commit 412463d0ced979d9219cf6409d674b6ce79f09a1

Update code to jdk 8

modules/cpr/src/main/java/org/atmosphere/util/annotation/InputStreamIterator.java
@@ -38,8 +38,8 @@ import java.util.LinkedList;
  */
 public final class InputStreamIterator {
 
-    private final Deque<InputStream> stack = new LinkedList<InputStream>();
-    private int rootCount;
+    private final Deque<InputStream> stack;
+    private final int rootCount;
     private InputStream current;
 
     /**
@@ -54,7 +54,8 @@ public final class InputStreamIterator {
      */
     public InputStreamIterator(final InputStream... filesOrDirectories) {
         addReverse(filesOrDirectories);
-        rootCount = stack.size();
+        stack = new LinkedList<>();
+        rootCount = 0;
     }
 
     /**

commit 30d8054b8306dea74c2e7bd424ad3964e5e9b26b

Fix NPE in InputStreamIterator
This broke in 412463d0ced979d9219cf6409d674b6ce79f09a1 when the field
initialization was moved from the field declaration to the constructor.

modules/cpr/src/main/java/org/atmosphere/util/annotation/InputStreamIterator.java
@@ -53,8 +53,8 @@ public final class InputStreamIterator {
      * in the specified order (depth first)
      */
     public InputStreamIterator(final InputStream... filesOrDirectories) {
-        addReverse(filesOrDirectories);
         stack = new LinkedList<>();
+        addReverse(filesOrDirectories);
         rootCount = 0;
     }
 